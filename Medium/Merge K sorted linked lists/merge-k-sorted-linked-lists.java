60ae1a49de10e1d21540b96f0adc7e913f489959
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-06-19 15:31:43 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution
+{
+    //Function to merge K sorted linked list.
+    Node mergeKList(Node[]arr,int K)
+    {
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        for(int i=0;i<K;i++)
+        {
+            while(arr[i]!=null)
+            {
+            pq.add(arr[i].data);
+            arr[i]=arr[i].next;
+            }
+        }
+        Node head=new Node(-1);
+        Node ans=head;
+        
+        while(!pq.isEmpty())
+        {
+            Node temp=new Node(pq.poll());
+            ans.next=temp;
+            ans=temp;
+        }
+        return head.next;
+    }
+}
