c899a2a52465cd8de3c64cde978d327232d85b9b
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-06-20 11:15:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse a string using Stack/reverse-a-string-using-stack.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            Solution obj = new Solution();
+            System.out.println(obj.reverse(sc.next()));
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    
+    public String reverse(String S){
+       Stack<Character> st=new Stack<>();
+         String ans="";
+         
+       for(int i=0;i<S.length();i++)
+       st.push(S.charAt(i));
+       
+       while(!st.isEmpty())
+       ans+=st.pop();
+       
+       return ans;
+    }
+}
