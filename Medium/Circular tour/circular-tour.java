c5b3918ba239802985dbaaf6335933bc49dda826
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-06-20 17:47:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Circular tour/circular-tour.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+class First_Circular_tour
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			sc.nextLine();
+			String str = sc.nextLine();
+			String arr[] = str.split(" ");
+			int p[] = new int[n];
+			int d[] = new int[n];
+			int j=0;
+			int k=0;
+			for(int i=0; i<2*n; i++)
+			{
+				if(i%2 == 0)
+				{
+					p[j]= Integer.parseInt(arr[i]);
+					j++;
+				}
+				else
+				{
+					d[k] = Integer.parseInt(arr[i]);
+					k++;
+				}
+			}
+			
+			System.out.println(new Solution().tour(p,d));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+
+// In java function tour() takes two arguments array of petrol
+// and array of distance
+
+class Solution
+{
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(int petrol[], int distance[])
+    {
+        int n=petrol.length;
+        int start=0,curr_petrol=0,prev_petrol=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            curr_petrol+=petrol[i]-distance[i];
+            
+            if(curr_petrol<0)
+            {
+                start=i+1;
+                prev_petrol+=curr_petrol;
+                curr_petrol=0;
+            }
+        }
+        return ((curr_petrol+prev_petrol)>=0)?(start):-1;
+    }
+}
