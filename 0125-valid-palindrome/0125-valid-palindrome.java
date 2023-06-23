6b321a683f434f7254deae741bdd32a31db8bf93
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-06-23 22:06:32 +0530

    Time: 25 ms (38.78%), Space: 44.2 MB (21.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,13 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        if (s.isEmpty()) 
-        	return true;
-        
-        int start = 0;
-        int last = s.length() - 1;
-        while(start <= last)
-        {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) 
-        		start++;
-        	 else if(!Character.isLetterOrDigit(currLast)) 
-        		last--;
-        	else
-            {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
-        }
-        return true;
-    }
+public boolean isPalindrome(String s)
+{
+s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+int start = 0, end = s.length()-1;
+while(start < end)
+{
+if(s.charAt(start++) != s.charAt(end--))
+return false;
+}
+return true;
+}
 }
