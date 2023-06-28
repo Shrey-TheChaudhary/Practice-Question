6bff52f786511d78253026cbcfff10c05a9ec50c
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-06-28 23:51:16 +0530

    Time: 54 ms (97.33%), Space: 44.3 MB (89.54%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+  var count = init;
+  return {
+    increment: () => {return ++count}, 
+    decrement: () => {return --count}, 
+    reset: () => {return count = init}
+  };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
