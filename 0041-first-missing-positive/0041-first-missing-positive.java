ba3fe5dd46f8a1a22c44627efb79e75283b39671
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-06-29 23:33:51 +0530

    Time: 1 ms (100.00%), Space: 59.5 MB (22.49%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,11 +1,24 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-    Arrays.sort(nums);
-    int ans = 1;
-    for(int i = 0; i < nums.length; i ++) {
-        if(nums[i] == ans)
-        ans ++;
-    }
-    return ans;
+    public int firstMissingPositive(int[] arr) {
+        int n = arr.length;
+        int i = 0;
+        while(i < n)
+        {
+            int correct = arr[i] - 1;
+            if(arr[i] > 0 && arr[i] < n && arr[i] != arr[correct])
+            {
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            }
+            else
+                i++;
+        }
+        for (int j = 0; j < n; j++) 
+        {
+            if(arr[j] != j + 1)
+                return j + 1;
+        }
+        return n + 1;
     }
 }
