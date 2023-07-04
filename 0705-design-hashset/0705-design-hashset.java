45554c74527ecd8bc976adbe07c564e529a9c892
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-07-04 17:45:46 +0530

    Time: 16 ms (89.26%), Space: 49.3 MB (62.23%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,24 +1,73 @@
 class MyHashSet {
-    boolean[] arr  ;
 
-    public MyHashSet() {
-        arr  = new boolean[1000001];
+    class Node{
+        int val = 0;
+        Node next = null;
+        Node(int v, Node n){
+            val = v;
+            next = n;
+        }
+    }
+    int hash = 500;
+    Node[] set = new Node[hash];
+    MyHashSet() {
+        
     }
     
-    public void add(int i) {
-        arr[i] = true;
+    void add(int key) {
+        if(set[key%hash]==null)
+            set[key%hash]=new Node(key,null);
+        
+        else{
+            Node temp = set[key%hash];
+            if(temp.val==key)
+                return;
+            while(temp.next!=null)
+            {
+                if(temp.val==key || temp.next.val==key)
+                    return;
+                temp=temp.next;
+            }
+            temp.next = new Node(key,null);
+        }
     }
     
-    public void remove(int i) {
-        arr[i] = false;
+    void remove(int key) {
+        if(set[key%hash]!=null)
+        {
+            Node temp = set[key%hash];
+            if(temp.val==key)
+            {
+                set[key%hash]=temp.next;
+                return;
+            }
+            while(temp.next!=null)
+            {
+                if(temp.next.val==key)
+                {
+                    temp.next = temp.next.next;
+                    return;
+                }
+                temp=temp.next;
+            }
+        }
     }
     
-    /** Returns true if this set contains the specified element */
-    public boolean contains(int i) {
-        return arr[i];
+    boolean contains(int key) {
+        if(set[key%hash]!=null)
+        {
+            Node temp = set[key%hash];
+            while(temp!=null)
+            {
+                if(temp.val==key)
+                    return true;
+                
+                temp=temp.next;
+            }
+        }
+        return false;
     }
 }
-
 /**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet obj = new MyHashSet();
