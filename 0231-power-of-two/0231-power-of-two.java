65be3893a40b423ace9d1509b7d243f5d10a308a
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-07-05 17:14:40 +0530

    Time: 1 ms (100.00%), Space: 39.7 MB (77.56%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0)
+            return false;
+        if((n&(n-1))==0)
+            return true;
+        
+        return false;
+    }
+}
