d36979012c0630fa5baff82f116fcf227e3d0709
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-07-08 13:18:15 +0530

    Time: 2 ms (81.66%), Space: 43.9 MB (58.27%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> list = new ArrayList<>();
         
-        combinationSum1(0, candidates, target, res, list);
-        return res;
+        List<List<Integer>> list = new ArrayList<>();
+        solve(0,candidates,new ArrayList<>(),list,target);
+        return list;
+        
     }
-    public static void combinationSum1(int index, int[] arr, int target, List<List<Integer>> res, List<Integer> list) {
+    
+    private void solve(int ind,int nums[],List<Integer> temp,List<List<Integer>> list,int target){
         
-        if(index == arr.length)
-        {
-            if(target == 0)
-                res.add(new ArrayList<>(list));
-            
+        if (target < 0 ) 
+            return;
+        
+        if (target == 0) {
+            list.add(new ArrayList<>(temp));
             return;
         }
-        if(arr[index] <= target){
-            //pick the index value
-            list.add(arr[index]);
-            combinationSum1(index, arr, target - arr[index], res, list);
-            list.remove(list.size() - 1);
+        
+        for (int i=ind;i<nums.length;i++){
+            
+            temp.add(nums[i]);
+            solve(i,nums,temp,list,target-nums[i]);
+            temp.remove(temp.size()-1);          
+            
         }
-        //not picking
-        combinationSum1(index + 1, arr, target, res, list);
+        
     }
 }
