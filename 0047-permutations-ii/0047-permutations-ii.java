64c3736b03b97925fc42f92e8caf709b226e1b0c
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-07-08 10:18:13 +0530

    Time: 935 ms (5.38%), Space: 43.7 MB (81.84%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        backtrack(ans, nums, new ArrayList<>(), visited);
+        return ans;
+    }
+
+    public void backtrack(List<List<Integer>> ans, int[] nums, List<Integer> temp, boolean[] visited) {
+        //for this problem, the base case will be if temp.size==nums.length
+        if (temp.size() == nums.length) 
+        {
+            if(!ans.contains(temp))
+            {
+            ans.add(new ArrayList<>(temp));
+            return;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            //if element is already exist, than skip that
+            if (!visited[i]) {
+                visited[i] = true;
+                temp.add(nums[i]);
+                backtrack(ans, nums, temp, visited);
+                temp.remove(temp.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
