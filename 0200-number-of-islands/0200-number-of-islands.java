38b0cee0ecd1473da90b85fabcdf2ca77968116a
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-07-12 21:46:40 +0530

    Time: 7 ms (21.11%), Space: 50.9 MB (19.07%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,55 @@
+class Pair {
+    int first;
+    int second; 
+    public Pair(int first, int second) {
+        this.first = first; 
+        this.second = second; 
+    }
+}
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length; 
+        int m = grid[0].length; 
+        int[][] vis = new int[n][m];
+        int cnt = 0; 
+        for(int row = 0; row < n ; row++) {
+            for(int col = 0; col < m ;col++) {
+                // if not visited and is a land
+                if(vis[row][col] == 0 && grid[row][col] == '1') {
+                    cnt++; 
+                    bfs(row, col, vis, grid); 
+                }
+            }
+        }
+        return cnt; 
+    }
+      private void bfs(int ro, int co, int[][] vis, char[][] grid) {
+      vis[ro][co] = 1; 
+      Queue<Pair> q = new LinkedList<Pair>();
+      q.add(new Pair(ro, co)); 
+      int n = grid.length; 
+      int m = grid[0].length; 
+      
+      // until the queue becomes empty
+      while(!q.isEmpty()) {
+          int row = q.peek().first; 
+          int col = q.peek().second; 
+          q.remove(); 
+          
+          int delrow[]={-1,0,1,0};
+          int delcol[]={0,1,0,-1};
+         for(int i=0;i<4;i++)
+         {
+                  int nrow = row+delrow[i]; 
+                  int ncol = col+delcol[i]; 
+
+        if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0) 
+        {
+                      vis[nrow][ncol] = 1; 
+                      q.add(new Pair(nrow, ncol)); 
+                  }
+              }
+          }
+      }
+}
+    
