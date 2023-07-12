62c71f83e3f1e9ce7388ad7b76284cbb9b81a0c7
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-07-12 16:13:16 +0530

    Time: 16 ms (53.92%), Space: 46.2 MB (37.88%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -9,57 +9,55 @@ class Node {
     }
 }
 class Solution {
-    public int[][] updateMatrix(int[][] grid) {
-         int n = grid.length; 
-	    int m = grid[0].length; 
-	    // visited and distance matrix
-	    int vis[][] = new int[n][m]; 
-	    int dist[][] = new int[n][m]; 
-	    // <coordinates, steps>
-	    Queue<Node> q = new LinkedList<Node>();
-	    // traverse the matrix
-	    for(int i = 0;i<n;i++) {
-	        for(int j = 0;j<m;j++) {
-	        // start BFS if cell contains 1
-	            if(grid[i][j] == 0) {
-	                q.add(new Node(i, j, 0)); 
-	                vis[i][j] = 1; 
-	            }
-	            else {
-	                // mark unvisted 
+    public int[][] updateMatrix(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        
+        int vis[][]=new int[n][m];
+        int dist[][]= new int[n][m];
+        
+        Queue<Node> q=new LinkedList<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    q.add(new Node(i,j,0));
+                    vis[i][j]=1;
+                }
+                else
 	                vis[i][j] = 0; 
-	            }
-	        }
-	    }
-	    
-	    
-	    
-	    int delrow[] = {-1, 0, +1, 0}; 
-	    int delcol[] = {0, +1, 0, -1}; 
-	    
-	    
-	    // n x m x 4 
-	    // traverse till queue becomes empty
-	    while(!q.isEmpty()) {
-	        int row = q.peek().f; 
-	        int col = q.peek().s; 
-	        int steps = q.peek().t; 
-	        q.remove(); 
-	        dist[row][col] = steps; 
-	        // for all 4 neighbours
-	        for(int i = 0;i<4;i++) {
-	            int nrow = row + delrow[i]; 
-	            int ncol = col + delcol[i]; 
-	            // check for valid unvisited cell
-	            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
-	            && vis[nrow][ncol] == 0)  {
-	                    vis[nrow][ncol] = 1; 
-    	            q.add(new Node(nrow, ncol, steps+1));  
-	            } 
-	            }
-	        }
-	    
-	    // return distance matrix
-	    return dist; 
+            }
+        }
+        int delrow[] = {-1,0,1,0}; 
+	    int delcol[] = {0,1,0,-1}; 
+        
+        //traverse queue
+        while(!q.isEmpty())
+        {
+            int row=q.peek().f;
+            int col=q.peek().s;
+            int steps=q.peek().t;
+            
+            q.remove();
+            
+            dist[row][col]=steps;
+            
+            for(int i=0;i<4;i++)
+            {
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0)
+                {
+                    vis[nrow][ncol]=1;
+                    q.add(new Node(nrow,ncol,steps+1));
+                }
+            }
+        }
+        return dist;
+        
     }
 }
