8a86458b4ca498e13749d44f07bedc593fd19a1f
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-07-13 14:18:54 +0530

    Time: 7 ms (51.78%), Space: 44.8 MB (47.58%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) 
+            adj.add(new ArrayList<>());
+
+        for (int i = 0; i < prerequisites.length; i++) 
+        adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        
+        int indegree[] = new int[numCourses];
+        for (int i = 0; i < numCourses; i++)
+        {
+            for (int it : adj.get(i)) 
+                indegree[it]++;
+            
+        }
+        Queue<Integer> q = new LinkedList<Integer>();
+        for (int i = 0; i < numCourses; i++) 
+        {
+            if (indegree[i] == 0) 
+                q.add(i);
+        }
+
+
+        int topo[] = new int[numCourses];
+        int ind = 0;
+        while (!q.isEmpty()) 
+        {
+            int node = q.peek();
+
+            q.remove();
+            topo[ind++] = node;
+
+            for (int it : adj.get(node))
+            {
+                indegree[it]--;
+                if (indegree[it] == 0) 
+                q.add(it);
+            }
+        }
+
+
+      if (ind == numCourses) 
+        return topo;
+        int[] arr = {};
+        return arr;
+    }
+}
