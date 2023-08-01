3db380a09f108bf37b8e3c4ce1d85dd3e67905af
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-08-01 18:44:27 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse words in a given string/reverse-words-in-a-given-string.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.reverseWords(s));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution 
+{
+    //Function to reverse words in a given string.
+    String reverseWords(String s)
+    {
+        String ans="";
+         StringTokenizer st = new StringTokenizer(s,".");
+       while (st.hasMoreTokens())
+       {
+           ans=st.nextToken()+"."+ans;
+       }
+     return ans.substring(0,s.length());
+    }
+}
