f226b0c866bccd5b73eb4600fb31460f9d658840
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-08-03 17:36:42 +0530

    Time: 1 ms (100.00%), Space: 40.6 MB (95.23%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,17 +1,25 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] dict = new int[26];
-        for (char ch : s.toCharArray()) {
-            dict[ch - 'a']++;
-        }
-        for (char ch : t.toCharArray()) {
-            dict[ch - 'a']--;
-        }
-        for (int i = 0; i < dict.length; i++) {
-            if (dict[i] < 0) {
-                return (char)('a' + i);
-            }
-        }
-        return ' ';
+//         int[] dict = new int[26];
+//         for (char ch : s.toCharArray()) {
+//             dict[ch - 'a']++;
+//         }
+//         for (char ch : t.toCharArray()) {
+//             dict[ch - 'a']--;
+//         }
+//         for (int i = 0; i < dict.length; i++) {
+//             if (dict[i] < 0) {
+//                 return (char)('a' + i);
+//             }
+//         }
+//         return ' ';
+        
+        char c = 0;
+        for(char cs : s.toCharArray()) 
+            c ^= cs;
+        for(char ct : t.toCharArray()) 
+            c ^= ct;
+        
+        return c;
     }
 }
