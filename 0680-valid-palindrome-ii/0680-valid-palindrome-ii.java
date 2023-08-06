cf5ad41e23b08fe167df31843d7bc700a001e1ab
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-08-06 10:16:05 +0530

    Time: 5 ms (98.30%), Space: 44.7 MB (11.92%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while(i <= j)
+        {
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else return ispalin(s,i+1,j) || ispalin(s,i,j-1);
+        }
+        return true;
+    }
+    public boolean ispalin(String s, int i, int j){
+        while(i <= j)
+        {
+            if(s.charAt(i) == s.charAt(j))
+            {
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
