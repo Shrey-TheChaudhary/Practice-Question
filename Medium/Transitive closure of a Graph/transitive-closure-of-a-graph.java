be75e4a6cc0772f436b2f6cd3cca303b3ea19bb5
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-17 09:11:19 +0530

    Updated Solution - GfG to GitHub

--- Medium/Transitive closure of a Graph/transitive-closure-of-a-graph.java
+++ Medium/Transitive closure of a Graph/transitive-closure-of-a-graph.java
@@ -33,79 +33,43 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-    static class Graph{
-       public int v;
-       public LinkedList<Integer>[] path;
-       public Graph(int v){
-           this.v = v;
-            path = new LinkedList[v];
-       }
-       
-       public void addPath(int graph[][]){
-           for(int i = 0; i < v; i++){
-               path[i] = new LinkedList<Integer>();
-               path[i].add(i);
-               for(int j = 0; j < v; j++){
-                   if(graph[i][j] == 1){
-                       path[i].add(j);
-                   }
-               } 
-           }
-       }
-       
-   }
-   
-   static ArrayList<ArrayList<Integer>> transitiveClosure(int N, int graph[][])
-   {
-       // code here
-       boolean[] visited = new boolean[N];
-       int[][] reached = new int[N][N];
-       
-       for(int i = 0; i < N; i++){
-          for(int j = 0; j < N; j++){
-            reached[i][j] = graph[i][j];
-          } 
-       }
-       
-       Graph graphObj = new Graph(N);
-       graphObj.addPath(graph);
-       
-       for(int i = 0; i < N; i++){
-           for(int j = 0; j < N; j++){
-               visited[j] = false;
-           }
-           
-           Queue<Integer> queue = new LinkedList<Integer>();
-           queue.add(i);
-           visited[i] = true;
-           
-           while(!queue.isEmpty()){
-               int peek = queue.peek();
-               queue.remove();
-               
-               reached[i][peek] = 1;
-               
-               for(int k = 0; k < graphObj.path[peek].size(); k++){
-                   if(!visited[graphObj.path[peek].get(k)]){
-                       queue.add(graphObj.path[peek].get(k));
-                       visited[graphObj.path[peek].get(k)] = true;
-                   }
-               }
-               
-           }
-       }
-       
-       ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-       for(int i = 0; i < N; i++){
-           ArrayList<Integer> local = new ArrayList<Integer>();
-          for(int j = 0; j < N; j++){
-            local.add(reached[i][j]);
-          } 
-          
-          result.add(local);
-       }
-       
-       return result;
-       
-   }
+     static ArrayList<ArrayList<Integer>> transitiveClosure(int N, int graph[][])
+    {
+        ArrayList<ArrayList<Integer>> transitive = new ArrayList<>();
+        for (int i = 0; i < N; i++){
+            ArrayList<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                temp.add(0);
+            }
+            transitive.add(temp);
+        }
+        
+        
+        for (int i = 0; i < N; i++){
+            boolean[] visited = new boolean[N];
+            Arrays.fill(visited, false);
+            
+            ArrayList<Integer> pathFrom = new ArrayList<>();
+            dfs(transitive, pathFrom, i, graph, visited);
+            
+            for (int node: pathFrom) {
+                transitive.get(i).set(node, 1);
+            }
+        }
+        
+        return transitive;
+        
+    }
+    
+    
+    static void dfs(ArrayList<ArrayList<Integer>> transitive, ArrayList<Integer> pathFrom, int node, int[][] graph, boolean[] visited) {
+        visited[node] = true;
+        pathFrom.add(node);
+        
+        for (int nbr = 0; nbr < graph[node].length; nbr++) {
+            if (!visited[nbr] && graph[node][nbr] == 1)
+                dfs(transitive, pathFrom, nbr, graph, visited);
+        }
+        
+    }
 }
