665cc89397ce381c8374b19625c4375f1382e69f
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-25 16:05:55 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (90.11%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -1,16 +1,17 @@
 class Solution {
-        public int kthGrammar(int N, int K) {
-        if(N==1) 
+    public int kthGrammar(int n, int k) {
+        if(n==1)
+        {
             return 0;
-
-        if(K%2==1) 
-            return kthGrammar(N-1, (K+1)/2);
-         else 
-            return flip(kthGrammar(N-1, K/2));
-        
-    }
-
-    public int flip(int val) {
-        return val==0? 1 : 0;
+        }
+        int parent = kthGrammar(n-1,(k+1)/2);
+        if(k%2 == 0 )
+        {
+            return parent==1?0:1;
+        }
+        else
+        {
+            return parent;
+        }
     }
 }
