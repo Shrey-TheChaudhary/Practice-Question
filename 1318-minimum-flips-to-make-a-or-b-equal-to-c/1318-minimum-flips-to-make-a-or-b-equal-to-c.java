6cb43373619028d30c8d2a7e491cc957eaa89bb5
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-26 13:36:39 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (35.85%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,5 +1,10 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
-        return Integer.bitCount(a & ~c) +  Integer.bitCount(~(a | b) & c) + Integer.bitCount(b & ~c);
+        //~means complement
+        return Integer.bitCount(a & ~c) // 0010 & 1010-->1
+            +  Integer.bitCount(~(a | b) & c) //(0010 | 0110)-> 0110-->~0110===1001 & 0101-->1
+            + Integer.bitCount(b & ~c); //0110 & 1010-->1
+        
+        //1+1+1=3 Ans
     }
 }
