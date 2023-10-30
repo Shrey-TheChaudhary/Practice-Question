443d31a5538468863d330e27ef061eb088d18f75
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-30 19:29:30 +0530

    Time: 0 ms (100.00%), Space: 49.5 MB (84.25%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,11 +1,34 @@
 class Solution {
+    //method 1 bit manupilation
     public int singleNonDuplicate(int[] nums) {
-       int n =nums.length;
-        int ans = 0;
-      
-        for (int i = 0; i < n; i++) 
-            ans = ans^nums[i];
-        
-        return ans; 
+                int n = nums.length;
+
+       
+        if (n == 1)
+            return nums[0];
+        if (nums[0]!=nums[1])
+            return nums[0];
+        if (nums[n-1]!=nums[n-2])
+            return nums[n-1];
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+                return nums[mid];
+            // We are in the left:
+            if ((mid % 2 == 1 && nums[mid]==nums[mid-1])|| (mid % 2 == 0 && nums[mid]==nums[mid+1])) {
+                // Eliminate the left half:
+                low = mid + 1;
+            }
+            // We are in the right:
+            else {
+                // Eliminate the right half:
+                high = mid - 1;
+            }
+        }
+       
+        return -1;
     }
 }
