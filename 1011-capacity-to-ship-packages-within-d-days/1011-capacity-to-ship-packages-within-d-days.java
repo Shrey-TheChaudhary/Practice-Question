b5575b4c36b19f504138f0a7fc9fc3012964552d
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-30 22:39:15 +0530

    Time: 9 ms (96.78%), Space: 45.2 MB (31.06%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,5 +1,44 @@
 class Solution {
+    // O(n)
+//      public static int findDays(int[] weights, int cap) {
+//         int days = 1; //First day.
+//         int load = 0;
+//         int n = weights.length; //size of array.
+//         for (int i = 0; i < n; i++) {
+//             if (load + weights[i] > cap) {
+//                 days += 1; //move to next day
+//                 load = weights[i]; //load the weight.
+//             } else {
+//                 //load the weight on the same day.
+//                 load += weights[i];
+//             }
+//         }
+//         return days;
+//     }
+
+//     public static int leastWeightCapacity(int[] weights, int d) {
+//         //Find the maximum and the summation:
+//         int low = Integer.MIN_VALUE, high = 0;
+//         for (int i = 0; i < weights.length; i++) {
+//             high += weights[i];
+//             low = Math.max(low, weights[i]);
+//         }
+
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             int numberOfDays = findDays(weights, mid);
+//             if (numberOfDays <= d) {
+//                 //eliminate right half
+//                 high = mid - 1;
+//             } else {
+//                 //eliminate left half
+//                 low = mid + 1;
+//             }
+//         }
+//         return low;
+//     }
      public int shipWithinDays(int[] weights, int days) {
+         //O(logn)
         int low=0;
         int high=0;
         for(int i:weights)
@@ -7,19 +46,16 @@ class Solution {
             low=Math.max(low,i);
             high+=i;
         }
-        int ans=0;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(capacity(weights,days,mid)){
-                
-                ans=mid;
+            if(capacity(weights,days,mid))     
                 high=mid-1;
-            }
-            else{
+            
+            else
                 low=mid+1;
-            }
+            
         }
-        return ans;
+        return low;
     }
 
     public boolean capacity(int[] weights,int days,int minw){
