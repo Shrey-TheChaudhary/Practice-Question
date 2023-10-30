05e870d4dd5e7456a1f2e64c2cde7c303f9fce72
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-30 10:08:46 +0530

    Time: 936 ms (64.99%), Space: 0B (100.00%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -8,3 +8,12 @@ AND
 tiv_2015 IN
 (SELECT tiv_2015 FROM Insurance
 GROUP BY tiv_2015 HAVING COUNT(pid)>1);
+
+# SELECT
+#     ROUND(SUM(tiv_2016), 2) as tiv_2016
+# FROM Insurance i
+# WHERE NOT EXISTS (
+#     SELECT * FROM Insurance i2 WHERE i2.pid != i.pid AND i2.lat = i.lat AND i2.lon = i.lon
+# ) AND tiv_2015 IN (
+#     SELECT tiv_2015 FROM Insurance i3 WHERE i3.pid != i.pid
+# )
