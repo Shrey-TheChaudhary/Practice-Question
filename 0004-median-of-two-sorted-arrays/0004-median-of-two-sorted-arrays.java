dfbe97a866559b99da75dccc3e9820a548f70068
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-31 17:39:03 +0530

    Time: 1 ms (100.00%), Space: 44.4 MB (82.65%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,33 +1,53 @@
+//Striver
+
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Size of two given arrays
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int n = n1 + n2;
-        int[] ans = new int[n];
 
-        int i=0, j=0, k=0;
+        int n = n1 + n2; //total size
+        //required indices:
+        int ind2 = n / 2;
+        int ind1 = ind2 - 1;
+        int cnt = 0;
+        int ind1el = -1, ind2el = -1;
 
-        while (i<=n1 && j<=n2) {
-            if (i == n1) {
-                while(j<n2) 
-                    ans[k++] = nums2[j++];
-                break;
-            } else if (j == n2) {
-                while (i<n1) 
-                    ans[k++] = nums1[i++];
-                break;
-            }
-
+        //apply the merge step:
+        int i = 0, j = 0;
+        while (i < n1 && j < n2) {
             if (nums1[i] < nums2[j]) {
-                ans[k++] = nums1[i++];
+                if (cnt == ind1) ind1el = nums1[i];
+                if (cnt == ind2) ind2el = nums1[i];
+                cnt++;
+                i++;
             } else {
-                ans[k++] = nums2[j++];
+                if (cnt == ind1) ind1el = nums2[j];
+                if (cnt == ind2) ind2el = nums2[j];
+                cnt++;
+                j++;
             }
         }
 
-        if (n%2==0) 
-            return (float)(ans[n/2-1] + ans[n/2])/2;
-        else 
-            return ans[n/2];
+        //copy the left-out elements:
+        while (i < n1) {
+            if (cnt == ind1) ind1el = nums1[i];
+            if (cnt == ind2) ind2el = nums1[i];
+            cnt++;
+            i++;
+        }
+        while (j < n2) {
+            if (cnt == ind1) ind1el = nums2[j];
+            if (cnt == ind2) ind2el = nums2[j];
+            cnt++;
+            j++;
+        }
+
+        //Find the median:
+        if (n % 2 == 1) {
+            return (double)ind2el;
+        }
+
+        return (double)((double)(ind1el + ind2el)) / 2.0;
     }
 }
