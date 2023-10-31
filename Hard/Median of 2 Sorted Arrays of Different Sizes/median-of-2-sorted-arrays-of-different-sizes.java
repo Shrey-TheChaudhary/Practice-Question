565fd8c5048b2061144fd220e7d55ec65cab449d
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-31 17:45:54 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Driver
+{
+    public static void main(String args[]) 
+	{ 
+	    Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int []a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            
+            int  m= sc.nextInt();
+            int []b = new int[m];
+            for (int i = 0; i < m; i++) b[i] = sc.nextInt();
+            
+            double res = new GFG().medianOfArrays(n, m, a, b);
+            
+            if (res == (int)res) System.out.println ((int)res);
+            else System.out.println (res);
+        }
+    		
+	} 
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class GFG 
+{ 
+    static double medianOfArrays(int n1, int n2, int a[], int b[]) 
+    {
+
+        int n = n1 + n2; //total size
+        //required indices:
+        int ind2 = n / 2;
+        int ind1 = ind2 - 1;
+        int cnt = 0;
+        int ind1el = -1, ind2el = -1;
+
+        //apply the merge step:
+        int i = 0, j = 0;
+        while (i < n1 && j < n2) {
+            if (a[i] < b[j]) {
+                if (cnt == ind1) ind1el = a[i];
+                if (cnt == ind2) ind2el = a[i];
+                cnt++;
+                i++;
+            } else {
+                if (cnt == ind1) ind1el = b[j];
+                if (cnt == ind2) ind2el = b[j];
+                cnt++;
+                j++;
+            }
+        }
+
+        //copy the left-out elements:
+        while (i < n1) {
+            if (cnt == ind1) ind1el = a[i];
+            if (cnt == ind2) ind2el = a[i];
+            cnt++;
+            i++;
+        }
+        while (j < n2) {
+            if (cnt == ind1) ind1el = b[j];
+            if (cnt == ind2) ind2el = b[j];
+            cnt++;
+            j++;
+        }
+
+        //Find the median:
+        if (n % 2 == 1) {
+            return (double)ind2el;
+        }
+
+        return (double)((double)(ind1el + ind2el)) / 2.0;
+    }
+}
