4c4a960e7f35e3d2357fcc8da62b4d2e7ab290fe
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-11-14 10:14:59 +0530

    Updated Solution - GfG to GitHub

--- Basic/Check if strings are rotations of each other or not/check-if-strings-are-rotations-of-each-other-or-not.java
+++ Basic/Check if strings are rotations of each other or not/check-if-strings-are-rotations-of-each-other-or-not.java
@@ -45,7 +45,7 @@ class Solution
     //Function to check if two strings are rotations of each other or not.
     public static boolean areRotations(String s1, String s2 )
     {
-        return (s2+s2).contains(s1);
+      return (s2+s2).contains(s1);
     }
     
 }
