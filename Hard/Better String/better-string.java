31218db4a6d7d44b8065292dc6e6e4963d645015
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-11-15 09:32:29 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Better String/better-string.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String str1 = sc.next();
+            String str2 = sc.next();
+
+            Solution obj = new Solution();
+            String ans = obj.betterString(str1, str2);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static String betterString(String str1, String str2) {
+        int count1 = countDistinctSubsequences(str1);
+        int count2 = countDistinctSubsequences(str2);
+
+        if (count1 >= count2) {
+            return str1;
+        } else {
+            return str2;
+        }
+    }
+
+    private static int countDistinctSubsequences(String str) {
+        int n = str.length();
+        int[] lastOccurrence = new int[256];
+        Arrays.fill(lastOccurrence, -1);
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1]) % 1000000007;
+
+            if (lastOccurrence[str.charAt(i - 1)] != -1) {
+                dp[i] = (dp[i] - dp[lastOccurrence[str.charAt(i - 1)] - 1] + 1000000007) % 1000000007;
+            }
+
+            lastOccurrence[str.charAt(i - 1)] = i;
+        }
+
+        return dp[n];
+    }
+}
