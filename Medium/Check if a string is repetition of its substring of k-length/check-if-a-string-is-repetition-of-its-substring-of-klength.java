195981d8e69c9d78193fb4ff89fe5aff4aecbbb0
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-12-29 09:53:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if a string is repetition of its substring of k-length/check-if-a-string-is-repetition-of-its-substring-of-klength.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            String s = sc.next();
+            int k = sc.nextInt();
+            int answer = new Solution().kSubstrConcat(n,s,k);
+    		System.out.println(answer);
+        }
+        
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+ int kSubstrConcat(int n, String s, int k)
+    {
+        int ans = 0;
+        if(n%k!=0) return ans;
+        HashSet<String> subs = new HashSet<>();
+        for(int i=0;i<n;i=i+k) subs.add(s.substring(i,i+k));
+        if(subs.size() <= 2) ans=1 ;    
+        return ans;
+    }
+}
