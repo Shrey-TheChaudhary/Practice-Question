b5811694c92eb96cbdc8a8251ec3d909811ab84d
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-01-12 09:31:29 +0530

    Time: 9 ms (25.54%), Space: 41.9 MB (24.13%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int count1 = 0, count2 = 0;
+        s = s.toLowerCase();
+        
+        for (int i = 0; i < s.length() / 2; i++) 
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') 
+                count1++;
+            
+        for (int i = s.length() / 2; i < s.length(); i++) 
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') 
+                count2++;
+            
+        return count1 == count2;
+    }
+}
