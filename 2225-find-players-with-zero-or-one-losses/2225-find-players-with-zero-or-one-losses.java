7d849edd5b4fc75d45bb03a6eb9e7fbafb7672e2
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-01-15 23:18:52 +0530

    Time: 86 ms (50.60%), Space: 91.5 MB (63.84%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> l1=new ArrayList<>();
-        List<Integer> l2=new ArrayList<>();
-        HashMap<Integer,Integer> winner=new HashMap<>();
-         HashMap<Integer,Integer> loser=new HashMap<>();
-        for(int i=0;i<matches.length;i++){
-            winner.put(matches[i][0],winner.getOrDefault(matches[i][0],0)+1);
-            loser.put(matches[i][1],loser.getOrDefault(matches[i][1],0)+1);
+        List<List<Integer>> ans = new ArrayList<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        // Update counts based on matches
+        for (int i = 0; i < matches.length; i++) {
+            hm.put(matches[i][1], hm.getOrDefault(matches[i][1], 0) + 1);
+            hm.put(matches[i][0], hm.getOrDefault(matches[i][0], 0));
         }
-        for(Map.Entry<Integer,Integer> entry:winner.entrySet()){
-            int a=entry.getKey();
-            if(!loser.containsKey(entry.getKey()))
-                l1.add(a);
+
+        // Initialize winner and loser lists
+        ans.add(new ArrayList<>());
+        ans.add(new ArrayList<>());
+
+        // Populate winners and losers lists
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            if (entry.getValue() == 0)
+                ans.get(0).add(entry.getKey());
+            else if (entry.getValue() == 1)
+                ans.get(1).add(entry.getKey());
         }
-         for(Map.Entry<Integer,Integer> entry:loser.entrySet()){
-            int a=entry.getKey();
-             if(loser.get(a)==1)
-                 l2.add(a);
-        }
-        ans.add(l1);
-        ans.add(l2);
-        for(int i=0;i<2;i++)
-            Collections.sort(ans.get(i));
+
+        // Sort the lists in ascending order
+        Collections.sort(ans.get(0));
+        Collections.sort(ans.get(1));
+
         return ans;
     }
 }
