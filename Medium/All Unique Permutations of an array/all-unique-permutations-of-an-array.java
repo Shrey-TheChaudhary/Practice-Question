9141730782a8eef67e451ea07e2fc5f580463395
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-01-17 09:08:45 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/All Unique Permutations of an array/all-unique-permutations-of-an-array.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            ArrayList<Integer> arr = new ArrayList<>();
+            
+            for(int i=0; i<n; i++)
+                arr.add(Integer.parseInt(S[i]));
+
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> res = ob.uniquePerms(arr,n);
+            for(int i=0; i<res.size(); i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    System.out.print(res.get(i).get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static ArrayList<ArrayList<Integer>> uniquePerms(ArrayList<Integer> arr , int n) {
+        Set<ArrayList<Integer>> ans = new HashSet<>();
+        solve(arr, 0, n - 1, ans);
+        
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>(ans);
+        result.sort((a, b) -> {
+            for (int i = 0; i < a.size(); i++) {
+                int cmp = a.get(i).compareTo(b.get(i));
+                if (cmp != 0) {
+                    return cmp;
+                }
+            }
+            return 0;
+        });
+        
+        return result;
+    }
+    static void solve(ArrayList<Integer> arr, int start, int end, Set<ArrayList<Integer>> ans) {
+        if (start == end) {
+            ans.add(new ArrayList<>(arr));
+            return;
+        } 
+        else {
+            for (int i = start; i <= end; i++) {
+                swap(arr, start, i);
+                solve(new ArrayList<>(arr), start + 1, end, ans);
+                swap(arr, start, i);
+            }
+        }
+    }
+
+    static void swap(ArrayList<Integer> arr, int i, int j) {
+        int temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
+};
