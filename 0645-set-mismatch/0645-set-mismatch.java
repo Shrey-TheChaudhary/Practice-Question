30cc98bbb373d51d0329e0b90f25a32da5203349
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-01-22 23:34:28 +0530

    Time: 2 ms (97.74%), Space: 45.7 MB (17.33%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,26 +1,45 @@
+// class Solution {
+//     public int[] findErrorNums(int[] nums) {
+        
+//         int dup = 0, miss = 0;
+//         HashSet<Integer> set = new HashSet<>();
+        
+//         // find duplicate using set
+//         for(int i = 0; i < nums.length; ++i)
+//         {
+//             if(set.contains(nums[i]))  
+//             dup = nums[i];
+            
+//             set.add(nums[i]);
+//         }
+        
+//         // find missing
+//         for(int i = 1; i <= nums.length; ++i){
+//             if(set.contains(i) == false){
+//                 miss = i;
+//                 break;
+//             }
+//         }
+        
+//         return new int[]{dup, miss};
+//     }
+// }
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        
-        int dup = 0, miss = 0;
-        HashSet<Integer> set = new HashSet<>();
-        
-        // find duplicate using set
-        for(int i = 0; i < nums.length; ++i)
-        {
-            if(set.contains(nums[i]))  
-            dup = nums[i];
-            
-            set.add(nums[i]);
+        int[] ans=new int[2];
+        int[] a=new int[10001];
+        for(int i=0;i<nums.length;i++){
+            if(a[nums[i]]==0)
+                a[nums[i]]=-1;
+            else
+                a[nums[i]]*=-1;
         }
-        
-        // find missing
-        for(int i = 1; i <= nums.length; ++i){
-            if(set.contains(i) == false){
-                miss = i;
-                break;
-            }
+        for(int i=1;i<=nums.length;i++){
+            if(a[i]==1)
+                ans[0]=i;
+            else if(a[i]==0)
+                ans[1]=i;
         }
-        
-        return new int[]{dup, miss};
+        return ans;
     }
 }
