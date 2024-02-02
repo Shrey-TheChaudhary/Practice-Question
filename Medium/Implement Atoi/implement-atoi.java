41d30f4b7c82bb602b0d82fdc3e25ccc0f91a1cd
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-02-02 09:48:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Implement Atoi/implement-atoi.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.Scanner;
+
+class aToi
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String str = sc.nextLine();
+			
+			Solution obj = new Solution();
+			int num = obj.atoi(str);
+			
+			System.out.println(num);
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+/*You are required to complete this method */
+class Solution
+{
+    int atoi(String s) {
+	// Your code here
+	int i=0;
+	    try{
+	        i = Integer.parseInt(s);
+	    }catch(Exception e){
+	        return -1;
+	    }
+	    return i;
+    }
+}
