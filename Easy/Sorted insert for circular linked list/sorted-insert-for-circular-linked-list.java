7964e665bf16c98088deff7e3aa4196c719b3438
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-02-05 08:42:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sorted insert for circular linked list/sorted-insert-for-circular-linked-list.java
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+// Node Class
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            Node start = null;
+            Node temp=null, r = null;
+
+            // Create linked list from the array arr[].
+            // Created linked list will be 1->2->11->56->12
+            if (n > 0) {
+                int arr = sc.nextInt();
+
+                temp = new Node(arr);
+                start = temp;
+                r = start;
+            }
+
+            for (int i = 0; i < n - 1; i++) {
+                int arr = sc.nextInt();
+                temp = new Node(arr);
+                r.next = temp;
+                r = r.next;
+            }
+
+            if (n > 0)
+            {
+                temp.next = start;
+                r = temp; 
+            }
+
+
+            int x = sc.nextInt();
+            Solution ob = new Solution();
+            start = ob.sortedInsert(start, x);
+            printList(start);
+            r = start;
+            while (r != start.next) {
+                temp = start;
+                start = start.next;
+                temp = null;
+            }
+            temp = null;
+        }
+    }
+
+    /* Function to print Nodes in a given linked list */
+    static void printList(Node start) {
+        Node temp;
+
+        if (start != null) {
+            temp = start;
+            do {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            } while (temp != start);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public Node sortedInsert(Node head, int data) {
+        Node newNode = new Node(data);
+        
+        if (head == null) {
+            newNode.next = newNode;
+            return newNode;
+        }
+        
+        Node curr = head;
+        Node next = curr.next;
+        
+        // Case 1: Insert at the beginning
+        if (data <= curr.data) {
+            newNode.next = head;
+            while (curr.next != head) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+            return newNode;
+        }
+        
+        // Case 2: Insert in the middle or at the end
+        while (next != head && data > next.data) {
+            curr = next;
+            next = next.next;
+        }
+        
+        curr.next = newNode;
+        newNode.next = next;
+        
+        return head;
+    }
+}
