621ec06da5b8ee56a734427c582b30a0db780ff5
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-02-22 10:53:43 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Distinct occurrences/distinct-occurrences.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Distinct_Occurrences
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String line = sc.nextLine();
+			String S = line.split(" ")[0];
+			String T = line.split(" ")[1];
+			
+			Solution ob = new Solution();
+			System.out.println(ob.subsequenceCount(S,T));
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+
+class Solution
+{
+    int  subsequenceCount(String s, String t)
+    {
+    // Your code here
+    int n = s.length();
+    int m  = t.length();
+ 
+    int mod = (int) (1e9 + 7);
+ 
+    int dp[][] = new int[n + 1][m + 1];
+    for(int i = 0;i <=n;i++)
+    {
+        dp[i][0] = 1;
+    }
+    for(int i = 1;i <=n;i++)
+    {
+        for(int j =1;j <=m;j++)
+        {
+            if(s.charAt(i -1) == t.charAt(j -1 ))
+            {
+                dp[i][j] = (dp[i -1][j -1] + dp[i -1][j] )%mod; 
+            }else
+            {
+                dp[i][j] = dp[i-1][j] %mod;
+            }
+        }
+    }
+    return dp[n][m];
+    }
+}
+
