78ec3b6da4999505d8844ad1d9dc9a5ce8e11785
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-02-27 09:44:56 +0530

    Updated Solution - GfG to GitHub

--- Easy/Play With OR/play-with-or.java
+++ Easy/Play With OR/play-with-or.java
@@ -52,12 +52,13 @@ class Solution{
     
    
     // Function for finding maximum and value pair
-    public static int[] game_with_number(int arr[], int n) {
+ public static int[] game_with_number(int arr[], int n) {
         for (int i = 0; i < n - 1; i++) {
             arr[i]=arr[i] |arr[i + 1];
         }
         return arr;
     }
+    
     
 }
 
