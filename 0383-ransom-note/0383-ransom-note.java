e60e9f81c0446be0ee3c03e3d46233e583e688bd
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-05 20:38:42 +0530

    Time: 2 ms (81.80%), Space: 44.6 MB (70.14%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,40 +1,40 @@
-// class Solution {
-//     public boolean canConstruct(String ransomNote, String magazine) {
-// 		if (ransomNote.length() > magazine.length()) return false;
-//         int[] alphabets_counter = new int[26];
-        
-//         for (char c : magazine.toCharArray())
-//             alphabets_counter[c-'a']++;
-
-//         for (char c : ransomNote.toCharArray()){
-//             if (alphabets_counter[c-'a'] == 0) return false;
-//             alphabets_counter[c-'a']--;
-//         }
-//         return true;
-//     }
-// }
-
-import java.util.*;
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> hm= new HashMap<>();
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
 
-        for(int i=0;i<magazine.length();i++)
-        { 
-            char ch=magazine.charAt(i);
-            hm.put(ch, hm.getOrDefault(ch,0)+1);
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
         }
-
-        for(int i=0;i<ransomNote.length();i++)
-        {
-        if(hm.containsKey(ransomNote.charAt(i)) &&                             hm.get(ransomNote.charAt(i))>0)
-    {
-        hm.put(ransomNote.charAt(i),hm.get(ransomNote.charAt(i))-1);
-            }
-            else 
-            return false;
-    }
-
         return true;
     }
 }
+
+// import java.util.*;
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         HashMap<Character, Integer> hm= new HashMap<>();
+
+//         for(int i=0;i<magazine.length();i++)
+//         { 
+//             char ch=magazine.charAt(i);
+//             hm.put(ch, hm.getOrDefault(ch,0)+1);
+//         }
+
+//         for(int i=0;i<ransomNote.length();i++)
+//         {
+//         if(hm.containsKey(ransomNote.charAt(i)) &&                             hm.get(ransomNote.charAt(i))>0)
+//     {
+//         hm.put(ransomNote.charAt(i),hm.get(ransomNote.charAt(i))-1);
+//             }
+//             else 
+//             return false;
+//     }
+
+//         return true;
+//     }
+// }
