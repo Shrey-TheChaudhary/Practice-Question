10f118ef8b67529dd43939a8f3bb46855d8a5c18
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-05 20:38:23 +0530

    Time: 16 ms (20.98%), Space: 44.6 MB (70.14%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,3 +1,19 @@
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+// 		if (ransomNote.length() > magazine.length()) return false;
+//         int[] alphabets_counter = new int[26];
+        
+//         for (char c : magazine.toCharArray())
+//             alphabets_counter[c-'a']++;
+
+//         for (char c : ransomNote.toCharArray()){
+//             if (alphabets_counter[c-'a'] == 0) return false;
+//             alphabets_counter[c-'a']--;
+//         }
+//         return true;
+//     }
+// }
+
 import java.util.*;
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
@@ -13,7 +29,7 @@ class Solution {
         {
         if(hm.containsKey(ransomNote.charAt(i)) &&                             hm.get(ransomNote.charAt(i))>0)
     {
-  hm.put(ransomNote.charAt(i),hm.get(ransomNote.charAt(i))-1);
+        hm.put(ransomNote.charAt(i),hm.get(ransomNote.charAt(i))-1);
             }
             else 
             return false;
