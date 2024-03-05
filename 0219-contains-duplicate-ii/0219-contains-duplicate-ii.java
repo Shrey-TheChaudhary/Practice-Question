00f64fa6afae0547bec78129565b97c30ea86fc8
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-05 20:54:01 +0530

    Time: 18 ms (80.24%), Space: 58 MB (20.79%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,17 +1,32 @@
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         if (k == 0) 
+//             return false;
+
+//     Map<Integer, Integer> hashMap = new HashMap<>();
+//     for (int i = 0; i < nums.length; i++) 
+//     {
+//         int integer = nums[i];
+// if (hashMap.containsKey(integer) && i - hashMap.get(integer) <= k)
+//             return true;
+        
+//           hashMap.put(integer, i);
+//     }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if (k == 0) 
-            return false;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-    Map<Integer, Integer> hashMap = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) 
-    {
-        int integer = nums[i];
-if (hashMap.containsKey(integer) && i - hashMap.get(integer) <= k)
-            return true;
-        
-          hashMap.put(integer, i);
-    }
-        return false;
+        for(int i=0; i<nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                int temp = Math.abs(map.get(nums[i]) - i);
+                if(temp <= k) return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;  
     }
 }
