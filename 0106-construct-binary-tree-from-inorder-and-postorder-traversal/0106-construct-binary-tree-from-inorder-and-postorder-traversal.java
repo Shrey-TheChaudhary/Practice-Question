732986a5f4295ea851cfe380d20d529ffa96a9a9
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-06 16:07:17 +0530

    Time: 0 ms (100.00%), Space: 44 MB (77.18%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,26 +15,26 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return build(inorder,0,inorder.length-1,postorder,0,postorder.length-1);
+        return build(inorder, inorder.length -1, 0, postorder, postorder.length -1);
     }
-    
-    public TreeNode build(int[] inorder, int inS, int inE, int[] postorder, int posS, int posE){
-        if(inS>inE || posS>posE) 
-            return  null;
-        
-        TreeNode root = new TreeNode(postorder[posE]);
-        
-        int rootI=0;
-        for(int i=0;i<inorder.length;i++){
-            if(inorder[i]==root.val){
-                rootI = i;
+
+    private TreeNode build(int[]inorder, int inStart, int inEnd, int[] postorder, int postStart){
+        if(postStart < 0 || inStart < inEnd){
+            return null;
+        }
+
+        TreeNode root = new TreeNode(postorder[postStart]);
+
+        int rIndex = inStart;
+        for(int i = inStart; i >= inEnd; i--){
+            if(inorder[i] == postorder[postStart]){
+                rIndex = i;
                 break;
             }
         }
-        
-        root.left = build(inorder,inS,rootI-1,postorder,posS,posS+rootI-inS-1);
-        root.right = build(inorder,rootI+1,inE,postorder,posS+rootI-inS,posE-1);
-        
+
+        root.right = build(inorder, inStart, rIndex + 1, postorder, postStart -1 );
+        root.left = build(inorder, rIndex -1, inEnd, postorder, postStart - (inStart - rIndex) -1);
         return root;
     }
 }
