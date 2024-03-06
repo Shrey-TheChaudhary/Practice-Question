ab82f2b2e91c351e9d162eea6ac1eceb8cc15c4d
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-06 23:17:51 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (38.64%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,9 +1,19 @@
 class Solution {
+    // public int trailingZeroes(int n) {
+    //     int res = 0;
+    //     for(int i = 5;i<=n;i=i*5){
+    //         res = res+n/i;
+    //     }
+    //     return res;
+    // }
     public int trailingZeroes(int n) {
-        int res = 0;
-        for(int i = 5;i<=n;i=i*5){
-            res = res+n/i;
-        }
-        return res;
+    int count = 0;
+
+    while(n > 0) {
+        n /= 5;
+        count += n;
     }
+
+    return count;
+}
 }
