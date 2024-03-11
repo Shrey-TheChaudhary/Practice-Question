7e383b77b74f371c387c05a5b5ae93f3992650b0
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-11 15:45:18 +0530

    Time: 1 ms (75.87%), Space: 41.5 MB (49.15%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -5,12 +5,12 @@ class Solution {
         for (int i = 0; i < s.length(); i++) 
             freq[s.charAt(i) - 'a']++;
         
-        String ans ="";
+        StringBuilder ans =new StringBuilder();
 
         for (int i = 0; i < order.length(); i++)
         {
             for (int j = 0; j < freq[order.charAt(i) - 'a']; j++) 
-               ans=ans+order.charAt(i);
+               ans.append(order.charAt(i));
             
             freq[order.charAt(i) - 'a'] = 0;
         }
@@ -18,8 +18,8 @@ class Solution {
         for (int i = 0; i < 26; i++)
         {
             for (int j = 0; j < freq[i]; j++) 
-                ans=ans+(char) ('a' + i);
+                ans.append((char) ('a' + i));
         }
-        return ans;
+        return ans.toString();
     }
 }
