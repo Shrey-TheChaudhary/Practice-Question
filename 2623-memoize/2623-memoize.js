81f38306117f92065a9c6dba5152e9bf8497fda0
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-11 22:23:37 +0530

    Time: 289 ms (69.35%), Space: 85.7 MB (94.81%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+  const cacheObject = {};
+  return function(...args) {
+    const key = JSON.stringify(args);
+    if (key in cacheObject) {
+      return cacheObject[key];
+    } else {
+      const result = fn(...args);
+      cacheObject[key] = result;
+      return result;
+    }
+  }
+}
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
