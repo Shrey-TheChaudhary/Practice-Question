c1d6c4bbe2540b85e8eac127c02745ed3bd1fd86
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-11 08:30:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String input[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(input[0]);
+            int x = Integer.parseInt(input[1]);
+            
+            int mat1[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat1[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+            int mat2[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat2[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+        
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countPairs(mat1,mat2,n,x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int countPairs(int mat1[][], int mat2[][], int n, int x) {
+        // code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int[] row : mat1){
+            for(int num : row)  map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        
+        int ans = 0;
+        for(int[] row : mat2){
+            for(int num : row)  if(map.containsKey(x-num))  ans++;
+        }
+        return ans;
+    }
+}
