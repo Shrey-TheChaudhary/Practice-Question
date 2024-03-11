36d3097aaba303aac3e46d4f50101e24591c708e
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-11 21:49:23 +0530

    Time: 4 ms (24.18%), Space: 41.8 MB (19.79%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,25 +1,33 @@
+import java.util.*;
 class Solution {
     public String customSortString(String order, String s) {
-        int[] freq = new int[26];
-        
-        for (int i = 0; i < s.length(); i++) 
-            freq[s.charAt(i) - 'a']++;
-        
-        StringBuilder ans =new StringBuilder();
-
-        for (int i = 0; i < order.length(); i++)
-        {
-            for (int j = 0; j < freq[order.charAt(i) - 'a']; j++) 
-               ans.append(order.charAt(i));
             
-            freq[order.charAt(i) - 'a'] = 0;
+// Create an array to store the order of characters
+        int[] charOrder = new int[26];
+
+        // Populate charOrder array with the positions of characters in 'order'
+        for (int i = 0; i < order.length(); i++) {
+            charOrder[order.charAt(i) - 'a'] = i;
         }
 
-        for (int i = 0; i < 26; i++)
-        {
-            for (int j = 0; j < freq[i]; j++) 
-                ans.append((char) ('a' + i));
+        // Convert string s to a Character array for sorting
+        Character[] charArray = new Character[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            charArray[i] = s.charAt(i);
         }
-        return ans.toString();
+
+        // Custom comparator based on the charOrder array
+        Comparator<Character> customComparator = Comparator.comparingInt(c -> charOrder[c - 'a']);
+
+        // Sort the Character array using the custom comparator
+        Arrays.sort(charArray, customComparator);
+
+        // Convert the sorted Character array back to a string
+        StringBuilder result = new StringBuilder();
+        for (Character c : charArray) {
+            result.append(c);
+        }
+
+        return result.toString();
     }
 }
