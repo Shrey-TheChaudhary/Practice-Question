5a356d44329299f7c22816d38df5ecff73482834
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-13 14:44:57 +0530

    Time: 1 ms (77.54%), Space: 40.3 MB (88.87%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,11 +1,35 @@
-public class Solution {
-    public int pivotInteger(int n) {
-        int sum = n * (n + 1) / 2;
+// public class Solution {
+//     public int pivotInteger(int n) {
+//         int sum = n * (n + 1) / 2;
       
-        int root = (int) Math.sqrt(sum);
-        if (root * root == sum) {
-            return root;
+//         int root = (int) Math.sqrt(sum);
+//         if (root * root == sum) {
+//             return root;
+//         }
+//         return -1;
+//     }
+// }
+class Solution {
+    public int pivotInteger(int n) {
+        int left = 1;
+        int right = n;
+        int sleft = left;
+        int sright = right;
+
+        if (n == 1) return n;
+
+        while (left < right) {
+            if (sleft < sright) 
+                sleft += ++left;
+             else 
+                sright += --right;
+            
+
+            if (sleft == sright && left + 1 == right - 1) {
+                return left + 1;
+            }
         }
+
         return -1;
     }
 }
