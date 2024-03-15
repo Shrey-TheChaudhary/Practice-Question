cfb952d41a798628dbdbbc913b3e8b4e20ac0fa2
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-15 09:09:52 +0530

    Time: 1873 ms (5.07%), Space: 47.2 MB (86.75%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,18 +1,32 @@
+// class Solution {
+//     public int numSubarraysWithSum(int[] nums, int goal) {
+//         int total = 0;
+//         int sum = 0;
+//         int presum[] = new int[nums.length + 1];
+//         presum[0] = 1;
+        
+//         for (int num : nums) {
+//             sum += num;
+//             if (sum >= goal) {
+//                 total += presum[sum - goal];
+//             }
+//             presum[sum]++;
+//         }
+        
+//         return total;
+//     }
+// }
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int total = 0;
-        int sum = 0;
-        int presum[] = new int[nums.length + 1];
-        presum[0] = 1;
-        
-        for (int num : nums) {
-            sum += num;
-            if (sum >= goal) {
-                total += presum[sum - goal];
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            for(int j=i;j<nums.length;j++){
+                sum+=nums[j];
+                if(sum==goal)
+                    c++;
             }
-            presum[sum]++;
         }
-        
-        return total;
+        return c;
     }
 }
