e0823f963c716a88a178796eb559d270d9ba82fa
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-18 00:02:30 +0530

    Updated Solution - GfG to GitHub

--- Easy/Level order traversal/level-order-traversal.java
+++ Easy/Level order traversal/level-order-traversal.java
@@ -129,26 +129,24 @@ class Solution
     //Function to return the level order traversal of a tree.
     static ArrayList <Integer> levelOrder(Node node) 
     {
-        ArrayList <Integer> ans=new ArrayList <Integer>();
-        Queue<Node> q=new LinkedList<Node>();
-        
+        ArrayList<Integer> ans=new ArrayList<>();
         if(node==null)
         return ans;
         
+        Queue<Node> q=new LinkedList<>();
         q.add(node);
         
-        while(!q.isEmpty())
-        {
-            Node curr=q.poll();
+        while(!q.isEmpty()){
+            Node curr=q.remove();
             ans.add(curr.data);
             
             if(curr.left!=null)
             q.add(curr.left);
+                
             if(curr.right!=null)
             q.add(curr.right);
         }
         return ans;
-        
     }
 }
 
