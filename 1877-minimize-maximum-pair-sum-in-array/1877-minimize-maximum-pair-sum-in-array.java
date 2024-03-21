5e7cc9705359eb53cdc2ea8e217e40dc20d25a14
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-03-21 12:39:41 +0530

    Time: 52 ms (90.93%), Space: 58.3 MB (37.32%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -1,26 +1,36 @@
 class Solution {
-    public int minPairSum(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int hash[] = new int[100001];
-        for(int num : nums) {
-            hash[num]++;
-            max = Math.max(max, num);
-            min = Math.min(min, num);
-        }
+     public int minPairSum(int[] nums) {
+//         int max = Integer.MIN_VALUE;
+//         int min = Integer.MAX_VALUE;
+//         int hash[] = new int[100001];
+//         for(int num : nums) {
+//             hash[num]++;
+//             max = Math.max(max, num);
+//             min = Math.min(min, num);
+//         }
 
-        int low = min;
-        int high = max;
-        max = Integer.MIN_VALUE;
-        while(low <= high) {
-            if(hash[low] == 0) low++;
-            else if(hash[high] == 0) high--;
-            else {
-                max = Math.max(max, low + high);
-                hash[low]--;
-                hash[high]--;
-            }
+//         int low = min;
+//         int high = max;
+//         max = Integer.MIN_VALUE;
+//         while(low <= high) {
+//             if(hash[low] == 0) low++;
+//             else if(hash[high] == 0) high--;
+//             else {
+//                 max = Math.max(max, low + high);
+//                 hash[low]--;
+//                 hash[high]--;
+//             }
+//         }
+//         return max;
+//     }
+         
+         Arrays.sort(nums);
+        int l=0,r=nums.length-1;
+        int max=Integer.MIN_VALUE;
+        while(l<r){
+            max=Math.max(max,nums[l++]+nums[r--]);
         }
         return max;
-    }
+     }
+    
 }
