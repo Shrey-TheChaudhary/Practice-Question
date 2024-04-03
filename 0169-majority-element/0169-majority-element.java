c6da704e50b975b81a39385a48f44e3398895c58
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-03 16:24:49 +0530

    Time: 2 ms (77.43%), Space: 53.1 MB (17.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -23,3 +23,21 @@ class Solution {
         return maxElement;
     }
 }
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n=nums.length;
+//         HashMap<Integer,Integer> hm=new HashMap<>();
+        
+//         for(int i:nums)
+//         {
+//             hm.put(i,hm.getOrDefault(i,0)+1);
+//         }
+//          for (Map.Entry<Integer, Integer> it : hm.entrySet()) {
+//             if (it.getValue() > (n / 2)) 
+//                 return it.getKey();
+//         }
+//         return -1;
+            
+//     }
+// }
