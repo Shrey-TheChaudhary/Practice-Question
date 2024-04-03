fd7ef694dd2af51c1efdffcd9fb8f17c2b6bd94a
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-03 21:15:30 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (68.16%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,17 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum_range = 0;
-        int sum_org = 0;
+        int totalsum = 0;
+        int arrsum = 0;
+        
         //Total sum
         for(int i =0;i<=nums.length;i++)
-            sum_range += i;
+            totalsum += i;
+        
         //sum of array
         for(int j =0;j<nums.length;j++)
-            sum_org += nums[j];
+            arrsum += nums[j];
         
-        return sum_range - sum_org;
+        return totalsum - arrsum;
         
     }
 }
