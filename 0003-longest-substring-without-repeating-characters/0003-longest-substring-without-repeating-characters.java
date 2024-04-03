89bf59cc55889d32fae49ab46a43a06c3041eff7
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-03 20:02:09 +0530

    Time: 4 ms (89.57%), Space: 44.5 MB (63.41%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,29 +1,46 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+//         int n = s.length();
+//         int maxLength = 0;
+        
+//         HashSet<Character> hs = new HashSet<>();
+//         int i = 0;
+        
+//         for (int j = 0; j < n; j++) 
+//         {
+//             if (!hs.contains(s.charAt(j))) 
+//             {
+//                 hs.add(s.charAt(j));
+//                 maxLength = Math.max(maxLength, j - i + 1);
+//             }
+//             else 
+//             {
+//                 while (hs.contains(s.charAt(j))) 
+//                 {
+//                     hs.remove(s.charAt(i));
+//                     i++;
+//                 }
+//                 hs.add(s.charAt(j));
+//             }
+//         }
+//         return maxLength;
+        
+        HashMap <Character,Integer> hm =new HashMap <Character,Integer>();
+
+        int left = 0, right = 0;
         int n = s.length();
-        int maxLength = 0;
+        int len = 0;
         
-        HashSet<Character> hs = new HashSet<>();
-        int i = 0;
-        
-        for (int j = 0; j < n; j++) 
+        while (right < n) 
         {
-            if (!hs.contains(s.charAt(j))) 
-            {
-                hs.add(s.charAt(j));
-                maxLength = Math.max(maxLength, j - i + 1);
-            }
-            else 
-            {
-                while (hs.contains(s.charAt(j))) 
-                {
-                    hs.remove(s.charAt(i));
-                    i++;
-                }
-                hs.add(s.charAt(j));
-            }
+            if (hm.containsKey(s.charAt(right))) 
+                left = Math.max(hm.get(s.charAt(right)) + 1, left);
+
+            hm.put(s.charAt(right), right);
+
+            len = Math.max(len, right - left + 1);
+            right++;
         }
-        
-        return maxLength;
-    }
+        return len;
+     } 
 }
