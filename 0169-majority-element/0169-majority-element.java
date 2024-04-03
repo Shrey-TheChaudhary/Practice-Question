c3db1edfaa73dc7e3802d064575200de6eec5a13
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-03 16:26:08 +0530

    Time: 1 ms (99.65%), Space: 53.2 MB (9.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,13 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //Moore’s Voting Algorithm:
-        //Initialize 2 variables:
-// Count –  for tracking the count of element
-// Element – for which element we are counting
-// Traverse through the given array.
-// - If Count is 0 then store the current element of the array as Element.
-// - If the current element and Element are the same increase the Count by 1.
-// - If they are different decrease the Count by 1.
+  
         
         int count = 0, maxElement = 0;
         for(int num: nums)
