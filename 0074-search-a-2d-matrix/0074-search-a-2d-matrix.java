f65f628888124c7b7455930443b956c4d2ac28a4
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-04 17:11:54 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (5.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,47 +1,47 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int n=matrix.length;
-        int m=matrix[0].length;
+    //public boolean searchMatrix(int[][] matrix, int target) {
+//         int n=matrix.length;
+//         int m=matrix[0].length;
 
-        int low=0,high=m-1;
-        while(low<n && high>=0)
-        {
-            if(matrix[low][high]==target)
-                return true;
-            
-            else if(matrix[low][high]>target)
-                high--;
-            else
-                low++;
-        }
-        return false;
-    }
-}
-    
-    //------------------Striver------------
-
-     // public boolean searchMatrix(int[][] matrix, int target) {
-     //      int n = matrix.length, m = matrix[0].length;
-     //        if (n == 0) 
-     //            return false; 
-         
-    // Apply binary search:
-//         int low = 0, high = n * m - 1;
-         
-//         while (low <= high)
+//         int low=0,high=m-1;
+//         while(low<n && high>=0)
 //         {
-//             int mid = (low + high) / 2;
-//             int row = mid / m;
-//             int col = mid % m;
-            
-//             if (matrix[row][col] == target) {
+//             if(matrix[low][high]==target)
 //                 return true;
-//             } else if (matrix[row][col] < target) {
-//                 low = mid + 1;
-//             } else {
-//                 high = mid - 1;
-//             }
+            
+//             else if(matrix[low][high]>target)
+//                 high--;
+//             else
+//                 low++;
 //         }
 //         return false;
 //     }
 // }
+    
+    //------------------Striver------------
+
+     public boolean searchMatrix(int[][] matrix, int target) {
+          int n = matrix.length, m = matrix[0].length;
+            if (n == 0) 
+                return false; 
+         
+    //Apply binary search:
+        int low = 0, high = n * m - 1;
+         
+        while (low <= high)  // 0 to 15(think like its a singly array)
+        {
+            int mid = (low + high) / 2;
+            int row = mid / m;
+            int col = mid % m;
+            
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
