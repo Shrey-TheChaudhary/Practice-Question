028a9338d6106cd3eed8c740e84e35c96bbbea7f
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-04 10:00:35 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (39.43%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -4,23 +4,9 @@
 //         if(nums.length==1)
 //             return 0;
         
-//         else
-//         {
-//             for(int i=0;i<nums.length;i++)
-//             {
-//             if(i==0)
-//             {
-//                 if(nums[i]>nums[i+1])
-//                     return i;
-//             }
-//             else if(i==nums.length-1)
-//             {
-//                 if(nums[i]>nums[i-1])
-//                     return i;
-//             }
-//             else if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
-//                 return i;
-//         }
+       // if ((i == 0||arr[i-1]<arr.get[i])&&(i == n-1||arr[i] > arr[i+1])) 
+       // {
+       //          return i;
 
 //         }
 //         return 0;
