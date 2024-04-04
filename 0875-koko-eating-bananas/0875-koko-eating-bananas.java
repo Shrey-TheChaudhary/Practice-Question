c84ae9b31f772f151f104f11a1ce987e269f2f6d
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-04 15:34:34 +0530

    Time: 28 ms (20.47%), Space: 45.1 MB (33.55%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,33 +1,57 @@
-class Solution {
+// class Solution {
     
-    //Striver
-    public int minEatingSpeed(int[] piles, int h) {
-        int max=0;
-        for(int x:piles)
-            max=Math.max(max,x);
-        int low = 1, high = max;
+//     //Striver
+//     public int minEatingSpeed(int[] piles, int h) {
+//         int max=0;
+//         for(int x:piles)
+//             max=Math.max(max,x);
+//         int low = 1, high = max;
 
-        //apply binary search:
-        while (low <= high)
-        {
-            int mid = (low + high) / 2;
-            int totalH = calculateTotalHours(piles, mid);
-            if (totalH <= h) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+//         //apply binary search:
+//         while (low <= high)
+//         {
+//             int mid = (low + high) / 2;
+//             int totalH = calculateTotalHours(piles, mid);
+//             if (totalH <= h) {
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return low;
+//     }
+//       public static int calculateTotalHours(int[] piles, int h) {
+//         int totalH = 0;
+//         int n = piles.length;
+//         //find total hours:
+//         for (int i = 0; i < n; i++) {
+//             totalH += Math.ceil((double)(piles[i])/(double)(h));
+//         }
+//         return totalH;
+    
+//     }
+// }
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 0;
+        for (int pile : piles) right = Math.max(right, pile);
+
+        while (left < right) {
+            int hoursUsed = 0;
+            int middle = (right + left) / 2;
+
+            for (int pile : piles) {
+                hoursUsed += Math.ceil((double) pile / middle);
+            }
+
+            if (hoursUsed <= h) {
+                right = middle;
+            }   else {
+                left = middle + 1;
             }
         }
-        return low;
-    }
-      public static int calculateTotalHours(int[] piles, int h) {
-        int totalH = 0;
-        int n = piles.length;
-        //find total hours:
-        for (int i = 0; i < n; i++) {
-            totalH += Math.ceil((double)(piles[i])/(double)(h));
-        }
-        return totalH;
-    
+
+        return right;
     }
 }
