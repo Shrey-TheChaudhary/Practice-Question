9fc887285d1e8c8158045824eb5839839e5073c1
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-08 13:10:40 +0530

    Time: 3 ms (80.94%), Space: 46.9 MB (54.67%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,52 +1,24 @@
-// class Solution {
-//     public int longestOnes(int[] nums, int k) {
-//         int i=0,j=0;
-//         int n=nums.length;
-//         int countZeroes=0,ans=Integer.MIN_VALUE;
-
-//         while(j<n) {
-//             if(nums[j]==0) 
-//                 countZeroes++;
-            
-//             if(countZeroes>k)
-//             {
-//                 if(nums[i]==0) 
-//                     countZeroes--;
-                
-//                 i++;
-//             }
-//             ans = Math.max(ans,j-i+1);
-//             j++;
-//         }
-//         return ans;
-
-//     }
-// }
-
-
-//STRIVER
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int count  = 0;
-        int l = 0;
-        int r = 0;
-        int max = 0;
-        int n = nums.length;
+        int l=0,r=0;
+        int n=nums.length;
+        int countZeroes=0,ans=Integer.MIN_VALUE;
 
-        while(r<n){
+        while(r<n) {
             if(nums[r]==0) 
-                count++;
-
-            if(count>k){
-                if(nums[l] == 0) 
-                    
-                    count--;
+                countZeroes++;
+            
+            while(countZeroes>k)  //if(countZeros>k)   4ms
+            {
+                if(nums[l]==0) 
+                    countZeroes--;
+                
                 l++;
             }
-
-            max = Math.max(max, r-l+1);
+            ans = Math.max(ans,r-l+1);
             r++;
         }
-        return max;
+        return ans;
+
     }
 }
