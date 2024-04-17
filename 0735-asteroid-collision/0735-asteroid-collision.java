06d1ab9ac494033eea3277df2903722b1825c99f
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-17 17:35:49 +0530

    Time: 1 ms (100.00%), Space: 44.8 MB (96.65%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -6,7 +6,9 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int asteroid = asteroids[i];
             while (j>0 && asteroids[j-1]>0 && asteroid<0 && asteroids[j-1] < Math.abs(asteroid)) 
-            {j--;}
+            {
+                j--;
+            }
 
             if (j==0 || asteroid>0 || asteroids[j-1]<0) 
                 asteroids[j++] = asteroid;
