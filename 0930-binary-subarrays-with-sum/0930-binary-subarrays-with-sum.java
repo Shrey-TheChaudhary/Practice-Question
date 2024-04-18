abcad7a15b1645f1c7d6495761f813022342c7ac
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-18 13:39:48 +0530

    Time: 3 ms (72.67%), Space: 48.4 MB (35.48%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,32 +1,32 @@
-// class Solution {
-//     public int numSubarraysWithSum(int[] nums, int goal) {
-//         int total = 0;
-//         int sum = 0;
-//         int presum[] = new int[nums.length + 1];
-//         presum[0] = 1;
-        
-//         for (int num : nums) {
-//             sum += num;
-//             if (sum >= goal) {
-//                 total += presum[sum - goal];
-//             }
-//             presum[sum]++;
-//         }
-        
-//         return total;
-//     }
-// }
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum==goal)
-                    c++;
+        int total = 0;
+        int sum = 0;
+        int presum[] = new int[nums.length + 1];
+        presum[0] = 1;
+        
+        for (int num : nums) {
+            sum += num;
+            if (sum >= goal) {
+                total += presum[sum - goal];
             }
+            presum[sum]++;
         }
-        return c;
+        
+        return total;
     }
 }
+// class Solution {
+//     public int numSubarraysWithSum(int[] nums, int goal) {
+//         int c=0;
+//         for(int i=0;i<nums.length;i++){
+//             int sum=0;
+//             for(int j=i;j<nums.length;j++){
+//                 sum+=nums[j];
+//                 if(sum==goal)
+//                     c++;
+//             }
+//         }
+//         return c;
+//     }
+// }
