f0a03b0a1b61147dedb9c5cf17c50ec1cc45c923
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-18 16:40:22 +0530

    Time: 7 ms (27.49%), Space: 44.3 MB (62.17%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,31 +8,63 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//        public ListNode mergeKLists(ListNode[] lists) {
+//         ListNode ans = null;
+//         for (ListNode list : lists) {
+//             ans = mergeTwoLists(ans, list);
+//         }
+//         return ans;
+//     }
+
+//     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//         if (l1 == null) 
+//             return l2;
+        
+//         if (l2 == null) 
+//             return l1;
+        
+//         if (l1.val < l2.val) 
+//         {
+//             l1.next = mergeTwoLists(l1.next, l2);
+//             return l1;
+//         } 
+//         else 
+//         {
+//             l2.next = mergeTwoLists(l1, l2.next);
+//             return l2;
+//         }
+//     }
+// }
+
 class Solution {
        public ListNode mergeKLists(ListNode[] lists) {
-        ListNode ans = null;
-        for (ListNode list : lists) {
-            ans = mergeTwoLists(ans, list);
-        }
-        return ans;
-    }
+        if(lists.length == 0) 
+            return null;
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) 
-            return l2;
-        
-        if (l2 == null) 
-            return l1;
-        
-        if (l1.val < l2.val) 
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for(ListNode node : lists)
         {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } 
-        else 
-        {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
+            while(node != null)
+            {
+                priorityQueue.add(node.val);
+                node = node.next;
+            }
         }
+
+        ListNode head;
+        if(!priorityQueue.isEmpty()) 
+            head = new ListNode(priorityQueue.poll());
+        else 
+            return null;
+        
+
+        ListNode temp = head;
+        while(!priorityQueue.isEmpty()){
+            temp.next = new ListNode(priorityQueue.poll());
+            temp = temp.next;
+        }
+        temp.next = null;
+        return head;
     }
 }
