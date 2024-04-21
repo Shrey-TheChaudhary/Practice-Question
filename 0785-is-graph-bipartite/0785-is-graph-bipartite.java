129a82926d8499812d52d741102dcdf0ccf32108
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-21 17:26:23 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (97.91%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -9,37 +9,55 @@ class Solution
            {
              if(color[i] == -1) 
              {
-               if(check(i, graph, color) == false) 
-                  return false;
+               // if(check(i, graph, color) == false) 
+               //    return false;
+                 
+                //DFS function 
+                 if(dfs(i, 0, color, graph) == false)
+                 return false; 
 
              }
            }
            return true;
     }
     
-     private boolean check( int start,int graph[][], int color[])
-      {
-         Queue<Integer> q = new LinkedList<Integer>();
-         q.add(start);
-         color[start] = 0;
-         while( !q. isEmpty()) 
-         {
-             int node = q.peek();
-             q. remove();
-             for(int it : graph[node])
-              {
-                 
-                 if(color[it] == -1) 
-                 {
-                     color[it] = 1 - color[node];
-                     q.add(it);
-                 }
-                   else if(color[it] == color[node]) 
-                     return false;
-                  
-              }
-         }  
-         return true;
-     }
-
+     // private boolean check( int start,int graph[][], int color[])
+     //  {
+     //     Queue<Integer> q = new LinkedList<Integer>();
+     //     q.add(start);
+     //     color[start] = 0;
+     //     while( !q. isEmpty()) 
+     //     {
+     //         int node = q.peek();
+     //         q. remove();
+     //         for(int it : graph[node])
+     //          {
+     //             if(color[it] == -1) 
+     //             {
+     //                 color[it] = 1 - color[node];
+     //                 q.add(it);
+     //             }
+     //               else if(color[it] == color[node]) 
+     //                 return false;
+     //          }
+     //     }  
+     //     return true;
+     // }
+    private boolean dfs(int node, int col, int[] color, int[][] adj) {
+    color[node] = col; 
+    
+    // Traverse adjacent nodes
+    for (int it = 0; it < adj[node].length; it++) {
+        // If uncolored
+        if (color[adj[node][it]] == -1) {
+            if (!dfs(adj[node][it], 1 - col, color, adj)) return false; 
+        }
+        // If previously colored and have the same color
+        else if (color[adj[node][it]] == col) {
+            return false; 
+        }
+    }
+    
+    return true; 
+}
 }
