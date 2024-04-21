4c6c038d37c75527e82adebf8c0d87033912c964
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-21 13:47:05 +0530

    Time: 23 ms (21.19%), Space: 45.5 MB (81.47%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -38,38 +38,45 @@ public class Codec {
     }
 
     public TreeNode deserialize(String data) {
-        if (data.isEmpty()) 
+        // Check if the serialized data is empty
+        if (data.isEmpty()) {
             return null;
-        
-        StringBuilder s = new StringBuilder(data);
+        }
 
+        // Use a StringBuilder to tokenize the serialized data
+        StringBuilder s = new StringBuilder(data);
+        // Read the root value from the serialized data
         int commaIndex = s.indexOf(",");
         String str = s.substring(0, commaIndex);
         s.delete(0, commaIndex + 1);
         TreeNode root = new TreeNode(Integer.parseInt(str));
 
-       
+        // Use a queue for level-order traversal
         Queue<TreeNode> q = new LinkedList<>();
-       
+        // Start with the root node
         q.offer(root);
-        
-        while (!q.isEmpty())
-        {
+
+        // Perform level-order traversal to reconstruct the tree
+        while (!q.isEmpty()) {
+            // Get the front node in the queue
             TreeNode node = q.poll();
+
+            // Read the value of the left child from the serialized data
             commaIndex = s.indexOf(",");
             str = s.substring(0, commaIndex);
             s.delete(0, commaIndex + 1);
-           
+            // If the value is not "#", create a new left child and push it to the queue
             if (!str.equals("#")) {
                 TreeNode leftNode = new TreeNode(Integer.parseInt(str));
                 node.left = leftNode;
                 q.offer(leftNode);
             }
 
+            // Read the value of the right child from the serialized data
             commaIndex = s.indexOf(",");
             str = s.substring(0, commaIndex);
             s.delete(0, commaIndex + 1);
-           
+            // If the value is not "#", create a new right child and push it to the queue
             if (!str.equals("#")) {
                 TreeNode rightNode = new TreeNode(Integer.parseInt(str));
                 node.right = rightNode;
