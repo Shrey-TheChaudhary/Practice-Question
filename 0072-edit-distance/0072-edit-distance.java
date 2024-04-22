057d1ec033d98422f5e2bce7cddbdef8be6dcef0
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-22 12:50:51 +0530

    Time: 5 ms (68.19%), Space: 44.4 MB (94.76%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -9,14 +9,19 @@ class Solution {
         for(int i=0;i<=m;i++)
             dp[0][i]=i;
         
-        for(int i=1;i<=n;i++)
+
+        for(int ind1=1;ind1<=n;ind1++)
         {
-            for(int j=1;j<=m;j++)
+            for(int ind2=1;ind2<=m;ind2++)
             {
-                if(word1.charAt(i-1)==word2.charAt(j-1))
-                    dp[i][j]=0+dp[i-1][j-1];
+                if(word1.charAt(ind1-1)==word2.charAt(ind2-1))
+                    dp[ind1][ind2]=0+dp[ind1-1][ind2-1];
                 else
-                    dp[i][j] = 1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                    // return 1+f(i-1,j) // Insertion of character.
+                    // return 1+f(i,j-1) // Deletion of character.
+                    // return 1+f(i-1,j-1) // Replacement of character.
+                dp[ind1][ind2]=1+Math.min
+                    (dp[ind1-1][ind2],Math.min(dp[ind1][ind2-1],dp[ind1-1][ind2-1]));
             }
         }
         return dp[n][m];
