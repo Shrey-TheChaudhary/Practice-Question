319ff5a6a303492cc92fb47b3075b9dc678028d3
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-23 13:32:59 +0530

    Time: 9 ms (10.06%), Space: 61.8 MB (6.17%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.java
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -3,10 +3,14 @@ class Solution {
          int n=stones.length;
         int dp[] = new int[n+1];
         int left=0,right=0;
-        for(int i=1;i<n;i++){
+        
+        for(int i=1;i<n;i++)
+        {
             left =Math.max(dp[i]+Math.abs(stones[i]-stones[i-1]),left);
+            
             if(i>1)
             right=Math.max(dp[i]+Math.abs(stones[i]-stones[i-2]),right);
+            
             dp[i]=Math.max(left,right);
             
         }
