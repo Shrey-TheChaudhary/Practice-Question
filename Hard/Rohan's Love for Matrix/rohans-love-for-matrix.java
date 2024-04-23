6906c68a5d2054bee3cd782196577b17132ec452
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-23 06:32:44 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Rohan's Love for Matrix/rohans-love-for-matrix.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.firstElement(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int firstElement(int n) {
+        int mod = 1000000007;
+        if(n <= 2)
+        {
+            return 1;
+        }
+        int one = 1;
+        int two = 1;
+        int sum = 0;
+        for(int i = 0 ; i < n -2; i++)
+        {
+            sum = (one + two) % mod;
+            one = two;
+            two = sum;
+        }
+        
+        return sum % mod;
+        // code here
+    }
+}
