1438b92a3c8d5cd14b63c93a8dd4263e8e707403
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-23 14:07:46 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (91.75%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -9,23 +9,29 @@ class Solution {
     
     return allpath(m-1, n-1, maze, dp);
 }
- static int allpath(int i, int j, int[][] maze, int[][] dp){
-    if (i == 0 && j == 0) {
-        if (maze[i][j] == 1) {
-            return 0;
+ static int allpath(int i, int j, int[][] maze, int[][] dp)
+ {
+        if (i == 0 && j == 0) 
+        {
+            if (maze[i][j] == 1) 
+                return 0;
+            else
+                return 1;
         }
-        return 1;
-    }
-    if(i<0 || j<0)return 0;
-    if(maze[i][j]==1)return 0;
+        if(i<0 || j<0)
+            return 0;
+     
+        if(maze[i][j]==1)
+            return 0;
 
-    if(dp[i][j]!=-1) return dp[i][j];
-    
-    int up = maze[i][j] + allpath(i-1, j, maze, dp);
-    int left = maze[i][j] + allpath(i, j-1, maze, dp);
-    
-    int count = up + left;
-    
-    return dp[i][j] = count;
+        if(dp[i][j]!=-1) 
+            return dp[i][j];
+
+        int up = maze[i][j] + allpath(i-1, j, maze, dp);
+        int left = maze[i][j] + allpath(i, j-1, maze, dp);
+
+        int count = up + left;
+
+        return dp[i][j] = count;
     } 
 }
