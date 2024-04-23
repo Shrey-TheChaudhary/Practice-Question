157f76656a22d5fc09cd34f1c457a69d6e491bbe
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-04-23 14:25:56 +0530

    Time: 1 ms (99.07%), Space: 43.9 MB (71.99%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,23 +1,49 @@
-class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-      int n = triangle.size();
-        int dp[][] = new int[n][n];
-        for( int [] row : dp ){
-            Arrays.fill( row , -1 );
-        }
-        for( int i = 0 ; i < n ; i++ ){
-            dp[n-1][i] = triangle.get(n-1).get(i);
-        }
-        for(int i=n-2 ;i>=0;i--)
-        {
-            for(int j=0;j<=i;j++)
-            {
-                int down=triangle.get(i).get(j)+dp[i+1][j];
-                int dig=triangle.get(i).get(j)+dp[i+1][j+1];
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//       int n = triangle.size();
+//         int dp[][] = new int[n][n];
+//         for( int [] row : dp ){
+//             Arrays.fill( row , -1 );
+//         }
+//         for( int i = 0 ; i < n ; i++ ){
+//             dp[n-1][i] = triangle.get(n-1).get(i);
+//         }
+//         for(int i=n-2 ;i>=0;i--)
+//         {
+//             for(int j=0;j<=i;j++)
+//             {
+//                 int down=triangle.get(i).get(j)+dp[i+1][j];
+//                 int dig=triangle.get(i).get(j)+dp[i+1][j+1];
                 
-            dp[i][j] = Math.min(down,dig);
-            }
-        }
-        return dp[0][0];
+//             dp[i][j] = Math.min(down,dig);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// }
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) 
+    {
+        int[][]val=new int[triangle.size()][triangle.size()];
+        
+        for(int row[]:val)
+         Arrays.fill(row,-10001);
+        
+        return minpathsum(triangle,0,0,val);    
+    }
+    static int minpathsum(List<List<Integer>>triangle,int i,int j,int[][]val)
+    {
+        if(i==triangle.size())
+         return 0;
+
+        if(val[i][j]!=-10001) 
+         return val[i][j];
+
+       int a= triangle.get(i).get(j)+minpathsum(triangle,i+1,j,val);
+       int b= triangle.get(i).get(j)+minpathsum(triangle,i+1,j+1,val);
+        
+       return val[i][j]=Math.min(a,b);
+
     }
 }
