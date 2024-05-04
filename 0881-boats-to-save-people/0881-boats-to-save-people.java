da8f88ed0c1d1e50fe4255a03422dc53d50fed97
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-05-04 10:41:08 +0530

    Time: 16 ms (97.17%), Space: 53.3 MB (79.23%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boats = 0;
+        Arrays.sort(people);
+        int i=0,j=people.length-1;
+        while(i<=j){
+            if((people[j]+people[i])<=limit){
+                i++;
+            }
+            j--;
+            boats++;
+        }
+        return boats;
+    }
+}
