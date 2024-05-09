f5bae7bbe23e7f35514142c799da6221a07398cd
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-05-10 00:01:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Combination Sum II/combination-sum-ii.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter ot = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String s[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+            List<List<Integer>> ans = new Solution().CombinationSum2(a, n, k);
+            for (List<Integer> list : ans) {
+                for (int x : list) ot.print(x + " ");
+                ot.println();
+            }
+            if (ans.size() == 0) ot.println();
+        }
+        ot.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+public List<List<Integer>> CombinationSum2(int a[], int n, int k){
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(a);
+        findCombinations(0,a,k,ans,new ArrayList<Integer>());
+        return ans;
+}
+ static void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
+        if (target == 0) {
+            ans.add(new ArrayList < > (ds));
+            return;
+        }
+ 
+        for (int i = ind; i < arr.length; i++) {
+            if (i > ind && arr[i] == arr[i - 1]) continue;
+            if (arr[i] > target) break;
+ 
+            ds.add(arr[i]);
+            findCombinations(i + 1, arr, target - arr[i], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+    
+    
+}
+}
