a2b04415281dd43dd1fe5fa4223f7e9ed442056c
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-05-25 19:52:44 +0530

    Time: 4 ms (87.21%), Space: 57.9 MB (44.43%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,21 +1,39 @@
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int left = 0, right = height.length - 1;
+//         int maxWater = 0;
+
+//         while (left < right) {
+//             // Calculate the area of the current container
+//             int area = Math.min(height[left], height[right]) * (right - left);
+//             maxWater = Math.max(maxWater, area);
+
+//             // Move the pointer pointing to the shorter line inward
+//             if (height[left] < height[right]) {
+//                 left++;
+//             } else {
+//                 right--;
+//             }
+//         }
+
+//         return maxWater;
+//     }
+// }
+
 class Solution {
     public int maxArea(int[] height) {
-        int left = 0, right = height.length - 1;
-        int maxWater = 0;
-
-        while (left < right) {
-            // Calculate the area of the current container
-            int area = Math.min(height[left], height[right]) * (right - left);
-            maxWater = Math.max(maxWater, area);
-
-            // Move the pointer pointing to the shorter line inward
-            if (height[left] < height[right]) {
-                left++;
+        int ans = 0, i = 0, j = height.length-1, res = 0;
+        while (i < j) {
+            if (height[i] <= height[j]) 
+            {
+                res = height[i] * (j - i);
+                i++;
             } else {
-                right--;
+                res = height[j] * (j - i);
+                j--;
             }
+            ans=Math.max(ans,res);
         }
-
-        return maxWater;
-    }
+        return ans;
+        }
 }
