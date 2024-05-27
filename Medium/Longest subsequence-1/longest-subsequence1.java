653f9414ee22ca7229f54cf5f5832bd68564034a
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-05-27 06:47:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest subsequence-1/longest-subsequence1.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] a = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            int res = obj.longestSubseq(n, a);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+class Solution{
+    static int longestSubseq(int n, int arr[]){
+        int dp[]= new int[n];
+        
+        for(int i=0;i<n;i++) 
+        dp[i]=1;
+        
+        dp[0]=1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+            if(Math.abs(arr[i]-arr[j])==1)
+            dp[i]=Math.max(dp[i],dp[j]+1);
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=Math.max(ans,dp[i]);
+        }
+        return ans;
+    }
+}
