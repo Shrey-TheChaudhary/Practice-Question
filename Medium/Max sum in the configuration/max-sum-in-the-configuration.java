c7d062313a44c9e8136cc73d2b539cd6fcd7113f
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-06-06 06:57:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Max sum in the configuration/max-sum-in-the-configuration.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Maxsum_Among_All_Rotations_Array {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+            System.out.println(new Solution().max_sum(arr, n));
+
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    long max_sum(int a[], int n) {
+        long curr = 0;
+        long total = 0;
+        
+        for (int i = 0; i < n; i++) {
+            curr += (long)i * a[i];
+            total += a[i];
+        }
+
+        long maxVal = curr;
+
+        for (int i = 1; i < n; i++) {
+            curr += total - (long)n * a[n - i];
+            if (curr > maxVal) {
+                maxVal = curr;
+            }
+        }
+
+        return maxVal;
+    }
+}
