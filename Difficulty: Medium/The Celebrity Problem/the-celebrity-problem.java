2d8d3a2edba1cc24d9996935a4d81eaa28f11c58
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-08-03 09:23:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -25,6 +25,7 @@ class GFG {
 
 
 // User function Template for Java
+
 class Solution {
     // Function to find if there is a celebrity in the party or not.
     public int celebrity(int mat[][]) {
