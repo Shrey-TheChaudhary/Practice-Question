bb67865197c0a9df1bd336a43c95983690a8f6f0
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-08-03 09:19:11 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int N = sc.nextInt();
+            int M[][] = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    M[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println(new Solution().celebrity(M));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to find if there is a celebrity in the party or not.
+    public int celebrity(int mat[][]) {
+        int n = mat.length;
+        
+        // Step 1: Find a candidate for celebrity
+        int candidate = 0;
+        for (int i = 1; i < n; i++) {
+            if (mat[candidate][i] == 1) {
+                candidate = i;
+            }
+        }
+        
+        // Step 2: Verify if the candidate is actually a celebrity
+        for (int i = 0; i < n; i++) {
+            if (i != candidate) {
+                if (mat[candidate][i] == 1 || mat[i][candidate] == 0) {
+                    return -1;
+                }
+            }
+        }
+        
+        return candidate;
+    }
+}
