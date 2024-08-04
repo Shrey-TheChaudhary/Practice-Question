5c5c6e336f67f8e8f7ae3fad8c3cb315ef8afd62
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-08-04 07:42:16 +0530

    Time: 66 ms (68.47%), Space: 46.5 MB (46.40%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,23 +1,46 @@
+// class Solution {
+//     public int rangeSum(int[] nums, int n, int left, int right) {
+//         List<Integer> arr = new ArrayList<>();
+//         for (int i = 0; i < n; i++) 
+//         {
+//             int prefix = 0;
+//             for (int j = i; j < n; j++)
+//             {
+//                 prefix += nums[j];
+//                 arr.add(prefix);
+//             }
+//         }
+        
+//         Collections.sort(arr);
+//         int result = 0;
+//         int MOD = 1000000007;
+        
+//         for (int i = left - 1; i < right; i++) 
+//             result = (result + arr.get(i)) % MOD;
+        
+//         return result;
+//     }
+// }
+
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-        List<Integer> arr = new ArrayList<>();
-        for (int i = 0; i < n; i++) 
-        {
+        int[] arr = new int[n * (n + 1) / 2];
+        for (int i = 0, k = 0; i < n; ++i) {
+            
             int prefix = 0;
-            for (int j = i; j < n; j++)
+            for (int j = i; j < n; ++j)
             {
                 prefix += nums[j];
-                arr.add(prefix);
+                arr[k++] = prefix;
             }
         }
+        Arrays.sort(arr);
+        int ans = 0;
+        int mod =1000000007;
         
-        Collections.sort(arr);
-        int result = 0;
-        int MOD = 1000000007;
+        for (int i = left - 1; i < right; ++i) 
+            ans = (ans + arr[i]) % mod;
         
-        for (int i = left - 1; i < right; i++) 
-            result = (result + arr.get(i)) % MOD;
-        
-        return result;
+        return ans;
     }
 }
