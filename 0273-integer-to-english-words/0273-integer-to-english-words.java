ca86c88256495e7c963bc470b6050348252feb2a
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-08-07 07:11:33 +0530

    Time: 13 ms (9.86%), Space: 42.3 MB (34.01%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,115 +1,30 @@
 class Solution {
-    public String numberToWords(int num) {
-        if(num==0){
-            return "Zero";
-        }
-        String[] ones={"","One ","Two ","Three ","Four ","Five ","Six ","Seven ","Eight ","Nine ","Ten ","Eleven ","Twelve ","Thirteen ","Fourteen ","Fifteen ","Sixteen ","Seventeen ","Eighteen ","Nineteen "};
-        String[] tens={"","Twenty ","Thirty ","Forty ","Fifty ","Sixty ","Seventy ","Eighty ","Ninety "};
-        String ans="";
+    private static final String[] to19 = "One,Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten,Eleven,Twelve,Thirteen,Fourteen,Fifteen,Sixteen,Seventeen,Eighteen,Nineteen".split(",");
+    private static final String[] tens = "Twenty,Thirty,Forty,Fifty,Sixty,Seventy,Eighty,Ninety".split(",");
+    private static final String[] units = {"", "Thousand", "Million", "Billion"};
 
-        if(num>=1000000000){
-            int temp=num/1000000000;
-            ans+=ones[temp]+"Billion ";
-            num%=1000000000;
-        }
-        if(num>=100000000){
-            int temp=num/1000000;
-                if(temp%100>=0){
-                    ans+=ones[temp/100]+"Hundred ";
-                }
-                temp%=100;
-                int count=0;
-                if(temp>=20){
-                    ans+=tens[temp/10-1];
-                }
-                if(temp<20){
-                    ans+=ones[temp];
-                    count=1;
-                }
-                temp%=10;
-                if(count==0){
-                    if(temp>0){
-                        ans+=ones[temp];
-                    }
-                }
-                ans+="Million ";
-                num%=1000000;
-        }
-        if(num>=10000000){
-            int temp=num/1000000;
-            if(temp>=20){
-                ans+=tens[temp/10-1];
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        return wordsHelper(num);
+    }
+
+    private String words(int n) {
+        if (n < 20) return to19[n - 1];
+        if (n < 100) return tens[n / 10 - 2] + (n % 10 > 0 ? " " + words(n % 10) : "");
+        if (n < 1000) return words(n / 100) + " Hundred" + (n % 100 > 0 ? " " + words(n % 100) : "");
+        return "";
+    }
+
+    private String wordsHelper(int n) {
+        int i = 0;
+        StringBuilder result = new StringBuilder();
+        while (n > 0) {
+            if (n % 1000 != 0) {
+                result.insert(0, words(n % 1000) + " " + units[i] + (result.length() > 0 ? " " : ""));
             }
-            temp%=100;
-            int count=0;
-                if(temp<20){
-                    ans+=ones[temp];
-                    count=1;
-                }
-                temp%=10;
-                if(count==0){
-                    if(temp>0){
-                        ans+=ones[temp];
-                    }
-                }
-            ans+="Million ";
-             num%=1000000;
+            n /= 1000;
+            i++;
         }
-        if(num>=1000000){
-            int temp=num/1000000;
-            ans+=ones[temp]+"Million ";
-            num%=1000000;
-        }
-        if(num>=100000){
-                int temp=num/1000;
-                if(temp>=100){
-                    ans+=ones[temp/100]+"Hundred ";
-                }
-                temp%=100;
-                if(temp>=20){
-                    ans+=tens[temp/10-1];
-                }
-                int count=0;
-                if(temp<20){
-                    ans+=ones[temp];
-                    count=1;
-                }
-                temp%=10;
-                if(count==0){
-                    if(temp>0){
-                        ans+=ones[temp];
-                    }
-                }
-                ans+="Thousand ";
-                num%=1000;
-            }
-        if(num>=10000){
-                int temp=num/1000;
-                if(temp>=20){
-                    ans+=tens[temp/10-1];
-                    temp%=10;
-                }
-                if(temp<20){
-                    ans+=ones[temp];
-                }
-                ans+="Thousand ";
-                num%=1000;
-            }
-        if(num>=1000){
-            ans+=ones[num/1000]+"Thousand ";
-            num%=1000;
-        }
-        if(num>=100){
-            ans+=ones[num/100]+"Hundred ";
-            num%=100;
-        }
-        if(num>=20){
-            ans+=tens[num/10-1];
-            num%=10;
-        }
-        if(num>0 && num<20){
-            ans+=ones[num];
-        }
-        return ans.trim();
+        return result.toString().trim();
     }
 }
