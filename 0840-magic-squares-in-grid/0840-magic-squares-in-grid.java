94ac071a036328e53e97fe436f4ba349bea85509
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-08-09 09:23:10 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (71.89%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -36,27 +36,25 @@ class Solution {
         int sum = grid[rowStart][colStart] + grid[rowStart][colStart + 1] + grid[rowStart][colStart + 2];
 
         // Check rows
-        for (int i = rowStart; i < rowStart + 3; i++) {
+        for (int i = rowStart; i < rowStart + 3; i++) 
+        {
             if (grid[i][colStart] + grid[i][colStart + 1] + grid[i][colStart + 2] != sum) 
-            {
                 return false;
-            }
         }
 
         // Check columns
         for (int j = colStart; j < colStart + 3; j++) {
-            if (grid[rowStart][j] + grid[rowStart + 1][j] + grid[rowStart + 2][j] != sum) {
+            if (grid[rowStart][j] + grid[rowStart + 1][j] + grid[rowStart + 2][j] != sum) 
                 return false;
-            }
         }
 
         // Check diagonals
-        if (grid[rowStart][colStart] + grid[rowStart + 1][colStart + 1] + grid[rowStart + 2][colStart + 2] != sum) {
+        if (grid[rowStart][colStart] + grid[rowStart + 1][colStart + 1] + grid[rowStart + 2][colStart + 2] != sum) 
             return false;
-        }
-        if (grid[rowStart][colStart + 2] + grid[rowStart + 1][colStart + 1] + grid[rowStart + 2][colStart] != sum) {
+        
+        if (grid[rowStart][colStart + 2] + grid[rowStart + 1][colStart + 1] + grid[rowStart + 2][colStart] != sum) 
             return false;
-        }
+        
 
         return true;
     }
