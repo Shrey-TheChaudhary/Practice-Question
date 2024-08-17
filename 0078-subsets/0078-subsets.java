02a57dd03f210071dd4de4c677cb162b80357e5c
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-08-17 13:02:12 +0530

    Time: 1 ms (52.99%), Space: 42.9 MB (11.11%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,49 +1,49 @@
 class Solution {
-//      public List<List<Integer>> subsets(int[] nums) {
+     public List<List<Integer>> subsets(int[] nums) {
          
-//         List<List<Integer>> res=new ArrayList<>();
-//         subsequence(0, nums, new ArrayList<>(), res);
-//         return res;
-//     }
+        List<List<Integer>> res=new ArrayList<>();
+        subsequence(0, nums, new ArrayList<>(), res);
+        return res;
+    }
     
-//    void subsequence(int ind , int[] nums , List<Integer> subset, List<List<Integer>> result){
+   void subsequence(int ind , int[] nums , List<Integer> subset, List<List<Integer>> result){
        
-//         if(ind >= nums.length )
-//         {
-//         result.add(new ArrayList<>(subset));
-//         return;
-//         }
-
-//         //for the current element 
-//         subset.add(nums[ind]);
-//         subsequence(ind +1, nums , subset ,result);
-
-//         //exclude the current element
-//         subset.remove(subset.size() - 1 );
-//         subsequence(ind +1 , nums,subset , result);
-
-//     }
-    
-    //--Using bit manupilation
-    
-    public List<List<Integer>> subsets(int[] nums) {
-        int n = nums.length;
-        int power=(int)Math.pow(2,n);
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for (int num = 0; num <power; num++) {
-            List<Integer> temp = new ArrayList<>();
-
-            for (int j = 0; j < n; j++) {
-                if ((num & (1 << j)) != 0) {
-                    temp.add(nums[j]);
-                }
-            }
-
-            ans.add(temp);
+        if(ind >= nums.length )
+        {
+        result.add(new ArrayList<>(subset));
+        return;
         }
 
-        return ans;
+        //for the current element 
+        subset.add(nums[ind]);
+        subsequence(ind +1, nums , subset ,result);
+
+        //exclude the current element
+        subset.remove(subset.size() - 1 );
+        subsequence(ind +1 , nums,subset , result);
+
     }
 }
+    //--Using bit manupilation
+    
+//     public List<List<Integer>> subsets(int[] nums) {
+//         int n = nums.length;
+//         int power=(int)Math.pow(2,n);
+//         List<List<Integer>> ans = new ArrayList<>();
+
+//         for (int num = 0; num <power; num++) {
+//             List<Integer> temp = new ArrayList<>();
+
+//             for (int j = 0; j < n; j++) {
+//                 if ((num & (1 << j)) != 0) {
+//                     temp.add(nums[j]);
+//                 }
+//             }
+
+//             ans.add(temp);
+//         }
+
+//         return ans;
+//     }
+// }
    
