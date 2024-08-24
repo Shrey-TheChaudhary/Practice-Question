aa29cf295b345d6d0974b337464a810552921dec
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-08-24 07:16:35 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class gfg {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int w = Integer.parseInt(br.readLine());
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            List<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] val = new int[array.size()];
+            int idx = 0;
+            for (int i : array) val[idx++] = i;
+
+            String lin = br.readLine();
+            String[] toke = lin.split(" ");
+            List<Integer> array1 = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : toke) {
+                array1.add(Integer.parseInt(token));
+            }
+
+            int[] wt = new int[array1.size()];
+            idx = 0;
+            for (int i : array1) wt[idx++] = i;
+
+            // calling method knapSack() of class Solution
+            System.out.println(new Solution().knapSack(w, wt, val));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[]) {
+     int N = val.length;
+        int[][] dp = new int[N + 1][W + 1];
+
+        // Build the dp array
+        for (int i = 1; i <= N; i++) {
+            for (int w = 0; w <= W; w++) {
+                // If weight of the current item is less than or equal to the current capacity
+                if (wt[i - 1] <= w) {
+                    // Choose the maximum of including the item or not including it
+                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - wt[i - 1]] + val[i - 1]);
+                } else {
+                    // If the item cannot be included, carry forward the value without it
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        
+    }
+     return dp[N][W];
+}
+}
