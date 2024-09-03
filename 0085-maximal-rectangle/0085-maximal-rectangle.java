521be4df811e3d674e0cb3aefb19b01ca7a8d2bd
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-09-03 17:23:38 +0530

    Time: 17 ms (50.27%), Space: 46.3 MB (62.89%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -29,43 +29,63 @@ class Solution {
                 return res;
                 
     }
-    public int largestRectangleArea(int[] heights) {
-        int maxarea = Integer.MIN_VALUE;
-        int nse[] = new int[heights.length]; //next smallest elemet
-        int pse[] = new int[heights.length]; //previous smallest element
-        Stack<Integer> s = new Stack<>();
-        int n = heights.length;
+//     public int largestRectangleArea(int[] heights) {
+//         int maxarea = Integer.MIN_VALUE;
+//         int nse[] = new int[heights.length]; //next smallest elemet
+//         int pse[] = new int[heights.length]; //previous smallest element
+//         Stack<Integer> s = new Stack<>();
+//         int n = heights.length;
 
-        // next smaller right
-        for (int i = n - 1; i >= 0; i--) {
-            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
-                s.pop();
-            }
-            if (s.isEmpty()) {
-                nse[i] = n;
-            } else {
-                nse[i] = s.peek();
-            }
-            s.push(i);
-        }
-        // next smaller left
-        s = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
-                s.pop();
-            }
-            if (s.isEmpty()) {
-                pse[i] = -1;
-            } else {
-                pse[i] = s.peek();
-            }
-            s.push(i);
+//         // next smaller right
+//         for (int i = n - 1; i >= 0; i--) {
+//             while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+//                 s.pop();
+//             }
+//             if (s.isEmpty()) {
+//                 nse[i] = n;
+//             } else {
+//                 nse[i] = s.peek();
+//             }
+//             s.push(i);
+//         }
+//         // next smaller left
+//         s = new Stack<>();
+//         for (int i = 0; i < n; i++) {
+//             while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+//                 s.pop();
+//             }
+//             if (s.isEmpty()) {
+//                 pse[i] = -1;
+//             } else {
+//                 pse[i] = s.peek();
+//             }
+//             s.push(i);
 
-        }
-        for (int i = 0; i < n; i++) {
+//         }
+//         for (int i = 0; i < n; i++) {
 
-            maxarea = Math.max((nse[i]-pse[i]-1)*heights[i], maxarea);
+//             maxarea = Math.max((nse[i]-pse[i]-1)*heights[i], maxarea);
+//         }
+//         return maxarea;
+//     }
+    static int largestRectangleArea(int histo[]) {
+        Stack < Integer > st = new Stack < > ();
+        int maxA = 0;
+        int n = histo.length;
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || histo[st.peek()] >= histo[i])) {
+                int height = histo[st.peek()];
+                st.pop();
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.peek() - 1;
+                maxA = Math.max(maxA, width * height);
+            }
+            st.push(i);
         }
-        return maxarea;
+        return maxA;
     }
+
 }
