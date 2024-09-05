c1237ab3fb2e1e90fa076d90036f63fd563c1c41
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-09-05 09:25:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -27,11 +27,10 @@ class GFG {
 
 // User function Template for Java
 class Solution {
-
     // Note that the size of the array is n-1
     int missingNumber(int n, int arr[]) {
 
-       int sum=0,expectedSum=n;
+     int sum=0,expectedSum=n;
         for(int i=0;i<n-1;i++){
             sum+=arr[i];
             expectedSum+=i+1;
