1083cd274f49319ed8e180b25d01bbdca45ce877
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-09-12 06:52:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG {
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            int ans = g.getMiddle(head);
+            System.out.println(ans);
+            // printList(head);
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    int getMiddle(Node head) {
+        if(head==null)return 0;
+        Node temp1=head;
+        Node temp2=head;
+        while(temp2.next!=null&&temp2.next.next!=null){
+           
+          
+        temp2=temp2.next.next; 
+        temp1=temp1.next;
+        }
+        if(temp2.next==null)  
+        return temp1.data;
+         if(temp2.next.next==null)
+         temp1=temp1.next;
+         
+        return temp1.data;
+    }
+}
