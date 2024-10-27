0fc232342e2df30fe4c78bfbb99c2d7e5a0a1fe8
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-10-27 08:21:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Triplet Family/triplet-family.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public boolean findTriplet(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num : arr){
+            set.add(num);
+        }
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = i+1; j < arr.length; j++)
+            {
+                int sum = arr[i]+arr[j];
+                if(set.contains(sum)) 
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            boolean res = obj.findTriplet(arr);
+            if (res)
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
