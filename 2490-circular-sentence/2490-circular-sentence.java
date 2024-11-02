85033fbd02cc133c388ef222e9001135c303d829
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-11-02 07:00:06 +0530

    Time: 1 ms (96.81%), Space: 42 MB (54.87%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -1,14 +1,41 @@
+// class Solution {
+//     public boolean isCircularSentence(String sentence) {
+//         int n = sentence.length();
+//         if(sentence.charAt(0) != sentence.charAt(n - 1))
+//             return false;
+        
+//         for(int i =0;i < n; i++)
+//         {
+//             if(sentence.charAt(i) == ' ' && sentence.charAt(i - 1) != sentence.charAt(i + 1)) 
+//                 return false;
+//         }
+//         return true;         
+//     }
+// }
+import java.util.StringTokenizer;
 class Solution {
-    public boolean isCircularSentence(String sentence) {
-        int n = sentence.length();
-        if(sentence.charAt(0) != sentence.charAt(n - 1))
+    public static boolean isCircularSentence(String sentence) {
+        StringTokenizer tokenizer = new StringTokenizer(sentence, " ");
+        
+       
+        if (!tokenizer.hasMoreTokens())
             return false;
         
-        for(int i =0;i < n; i++)
+        String firstWord = tokenizer.nextToken();
+        String previousWord = firstWord;
+        
+        while (tokenizer.hasMoreTokens()) 
         {
-            if(sentence.charAt(i) == ' ' && sentence.charAt(i - 1) != sentence.charAt(i + 1)) 
+            String currentWord = tokenizer.nextToken();
+            
+            if (previousWord.charAt(previousWord.length() - 1) != currentWord.charAt(0)) 
                 return false;
+            
+            // Update previousWord for the next iteration
+            previousWord = currentWord;
         }
-        return true;         
+        
+        // Finally, check if the last character of the last word matches the first character of the first word
+        return previousWord.charAt(previousWord.length() - 1) == firstWord.charAt(0);
     }
 }
