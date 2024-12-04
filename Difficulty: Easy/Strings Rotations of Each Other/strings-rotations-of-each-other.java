d05d4f745b5ab9fb60e2808d5c0e809c1ad08a91
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-12-04 06:34:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        // taking input using BufferedReader class
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // taking total count of testcases
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            // Reading the two Strings
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+
+            // Creating an object of class Rotate
+            Solution obj = new Solution();
+
+            // calling areRotations method
+            // of class Rotate and printing
+            //"1" if it returns true
+            // else "0"
+            if (obj.areRotations(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    // Function to check if two strings are rotations of each other or not.
+    public static boolean areRotations(String s1, String s2) {
+        // Your code here
+        if (s1.length() != s2.length()) return false;
+        String concatenated = s1 + s1;
+        return kmpSearch(concatenated, s2);
+    }
+    private static boolean kmpSearch(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
+        int[] lps = computeLPSArray(pattern);
+        int i = 0, j = 0;
+        while (i < n) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+                if (j == m) return true;
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+    private static int[] computeLPSArray(String pattern) {
+        int m = pattern.length();
+        int[] lps = new int[m];
+        int len = 0;
+        int i = 1;
+        while (i < m) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
