bc879cceb6d2032c3d9aea2a79641565d1b8afcc
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-12-05 07:01:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -32,7 +32,7 @@ class GFG {
 class Solution {
     // Function to sort an array of 0s, 1s, and 2s
     public void sort012(int[] arr) {
-         Arrays.sort(arr);
+       Arrays.sort(arr);
     }
 }
 
