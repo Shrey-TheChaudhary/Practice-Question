e8ba469f7721a5b1ffc34058eba51b62734dfdd3
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-12-14 07:22:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
@@ -42,7 +42,7 @@ public class GFG {
 
 class Solution {
     int search(int[] arr, int key) {
-       for(int i=0;i<arr.length;i++)
+        for(int i=0;i<arr.length;i++)
             if(arr[i]==key)
                 return i;
         return -1;
