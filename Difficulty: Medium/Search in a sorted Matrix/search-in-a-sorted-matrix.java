cb10445e067dd2d86a911b49dc9311eb88821f8b
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-12-24 07:05:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t > 0) {
+            int rows = sc.nextInt();
+            int columns = sc.nextInt();
+
+            int matrix[][] = new int[rows][columns];
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            int target = sc.nextInt();
+
+            Solution x = new Solution();
+
+            if (x.searchMatrix(matrix, target))
+                System.out.println("true");
+            else
+                System.out.println("false");
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchMatrix(int[][] mat, int x) {
+         for(int i[]:mat) for(int j:i) if(j == x) return true;
+        return false;
+    }
+}
