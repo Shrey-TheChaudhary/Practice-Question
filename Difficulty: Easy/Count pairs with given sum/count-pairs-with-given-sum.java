13e4e10fedbbab8166f94a533261edda5275cc57
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2024-12-27 06:48:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count pairs with given sum/count-pairs-with-given-sum.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+
+import java.util.HashMap;
+
+class Solution {
+    int countPairs(int arr[], int target) {
+        // HashMap to store the frequency of elements
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int num : arr) {
+            // Check if the complement exists in the map
+            int complement = target - num;
+            if (map.containsKey(complement)) {
+                count += map.get(complement); // Add the count of the complement
+            }
+
+            // Add the current number to the map
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+}
+
+//{ Driver Code Starts.
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int target = Integer.parseInt(read.readLine());
+
+            Solution obj = new Solution();
+
+            System.out.println(obj.countPairs(nums, target));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
