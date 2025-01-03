a42a3cb9da7328414b0b1dee1015e2d5da6e3631
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-01-03 06:54:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String s[] = br.readLine().split(" ");
+            int arr[] = new int[s.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.parseInt(s[i]);
+            }
+            int k = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            System.out.println(obj.subarrayXor(arr, k));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public long subarrayXor(int arr[], int k) {
+       HashMap<Integer, Integer> mp = new HashMap<>();
+        int xor = 0; 
+        long cnt = 0; 
+
+        // Initialize the map for subarrays starting from the beginning
+        mp.put(0, 1); 
+
+        for (int a : arr) {
+            xor = xor ^ a; // Update current XOR
+
+            // Check if there's a prefix with XOR equal to (xor ^ k)
+            if (mp.containsKey(xor ^ k)) {
+                cnt += mp.get(xor ^ k); 
+            }
+
+            // Add the current XOR to the map
+            mp.put(xor, mp.getOrDefault(xor, 0) + 1); 
+        }
+
+        return cnt; 
+    }
+}
