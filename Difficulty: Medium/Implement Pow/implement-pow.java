d6caf68e25ff9ebe4a878137d928616f74d98b7e
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-01-29 06:52:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Pow/implement-pow.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            double b = sc.nextDouble();
+            int e = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.printf("%.5f\n", ob.power(b, e));
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    double power(double b, int e) {
+        return (double) Math.pow(b,e);
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
