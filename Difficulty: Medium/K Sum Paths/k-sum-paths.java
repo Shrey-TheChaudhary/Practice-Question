ca9b3af4468b92a5ad353e9dfae61fe27847f0d6
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-02-10 07:04:39 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sum Paths/k-sum-paths.java
@@ -0,0 +1,126 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GFG {
+    static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.equals('N')) return null;
+        String[] s = str.split(" ");
+
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (!q.isEmpty() && i < s.length) {
+            // Get and remove the front of the queue
+            Node currNode = q.remove();
+
+            // Get the current node's value from the string
+            String currVal = s[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= s.length) break;
+            currVal = s[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.right);
+            }
+
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            int k = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+
+            System.out.println(ob.sumK(root, k));
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public int sumK(Node root, int k) {
+        Map<Integer , Integer> map = new HashMap<>();
+        map.put(0 , 1);
+        return get(root , map , 0 , k);
+    }
+    private int get(Node root , Map<Integer , Integer> map , int sum , int k){
+        if(root == null) return 0;
+        sum += root.data;
+        int ans = 0;
+        if(map.containsKey(sum - k)){
+            ans += map.get(sum - k);
+        }
+        map.put(sum , map.getOrDefault(sum , 0) + 1);
+        ans += get(root.left , map , sum , k);
+        ans += get(root.right , map , sum , k);
+        map.put(sum , map.get(sum) - 1);
+        if(map.get(sum) == 0) {
+            map.remove(sum);
+        }
+        return ans;
+    }
+}
