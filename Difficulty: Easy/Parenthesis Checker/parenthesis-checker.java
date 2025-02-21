cd15073dbf0586e3d3a1e3932e76a99889b54fa8
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-02-21 06:48:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,30 +1,28 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
-class Driverclass
-{
-    public static void main(String args[])
-    {
+class Driverclass {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
-        //Reading total number of testcases
-        int t= sc.nextInt();
-        
-        while(t-- >0)
-        {
-            //reading the string
+
+        // Reading total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // reading the string
             String st = sc.next();
-            
-            //calling ispar method of Paranthesis class 
-            //and printing "balanced" if it returns true
-            //else printing "not balanced"
-            if(new Solution().ispar(st) == true)
-                System.out.println("balanced");
+
+            // calling ispar method of Paranthesis class
+            // and printing "balanced" if it returns true
+            // else printing "not balanced"
+            if (new Solution().isBalanced(st) == true)
+                System.out.println("true");
             else
-                System.out.println("not balanced");
-        
+                System.out.println("false");
+
+            System.out.println("~");
         }
     }
 }
@@ -33,33 +31,20 @@ class Driverclass
 
 
 class Solution {
-    // Function to check if brackets are balanced or not.
-    static boolean ispar(String x) {
-        Map<Character, Character> map = new HashMap<>();
-        Stack<Character> stack = new Stack<>();
+    static boolean isBalanced(String s) {
+        Stack<Character> st = new Stack<>();
         
-        // Initialize the mapping of closing brackets to their corresponding opening brackets
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
-        
-        for (int i = 0; i < x.length(); i++) {
-            char curr = x.charAt(i);
-            
-            // If the current character is a closing bracket
-            if (map.containsKey(curr)) {
-                // Check if the stack is empty or the top of the stack does not match the expected opening bracket
-                if (stack.isEmpty() || stack.pop() != map.get(curr)) {
-                    return false;
-                }
-            } else {
-                // If it's an opening bracket, push it onto the stack
-                stack.push(curr);
+        for(int i = 0 ; i< s.length() ; i++){
+            if(st.isEmpty()) st.push(s.charAt(i));
+            else if(st.peek() == '{' && s.charAt(i) == '}') st.pop();
+            else if(st.peek() == '(' && s.charAt(i) == ')') st.pop();
+            else if(st.peek() == '[' && s.charAt(i) == ']') st.pop();
+            else{
+                st.push(s.charAt(i));
             }
         }
         
-        // If the stack is empty, all brackets were properly balanced
-        return stack.isEmpty();
+        if(st.isEmpty()) return true;
+        return false;
     }
 }
-
