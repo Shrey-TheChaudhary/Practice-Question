03b6206c8a6be44ff4468672054d5ffe119efecd
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-02-23 08:46:21 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            ArrayList<Integer> result = new Solution().nextLargerElement(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();
+                System.out.println("~");
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the next greater element for each element of the array.
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+      ArrayList<Integer>res=new ArrayList<>();
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int j=i+1;
+            while(j<arr.length)
+            {
+                if(arr[j]>arr[i])
+                {
+                    res.add(arr[j]);
+                    break;
+                }
+                j++;
+            }
+            if(j==arr.length)
+            res.add(-1);
+        }
+        res.add(-1);
+        return res;
+    }
+}
