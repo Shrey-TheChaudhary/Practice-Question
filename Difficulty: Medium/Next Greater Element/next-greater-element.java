c9c567f80827cdbb5a02bc6ea9d55e909f0e082b
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-02-23 08:50:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.java
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -38,7 +38,7 @@ class Geeks {
 class Solution {
     // Function to find the next greater element for each element of the array.
     public ArrayList<Integer> nextLargerElement(int[] arr) {
-       ArrayList<Integer>res=new ArrayList<>();
+        ArrayList<Integer>res=new ArrayList<>();
         for(int i=0;i<arr.length-1;i++)
         {
             int j=i+1;
