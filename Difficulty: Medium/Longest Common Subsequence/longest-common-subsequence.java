4ef80efa9a9da33c2a408cc394e77d42a0a2d342
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-03-06 06:47:37 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while (test-- > 0) {
+            String s1 = sc.next(); // Take both the strings as input
+            String s2 = sc.next();
+
+            Solution obj = new Solution();
+
+            // Call the lcs function with the lengths of the strings as
+            // parameters
+            System.out.println(obj.lcs(s1, s2));
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static int lcs(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
