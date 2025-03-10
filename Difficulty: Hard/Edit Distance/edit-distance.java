a3087bf72ce9de83c313c77904b6048c55b4bf9d
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-03-10 09:36:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.java
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -25,25 +25,19 @@ class GFG {
 
 class Solution {
     public int editDistance(String s1, String s2) {
-        
         int n = s1.length();
         int m = s2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i =0; i <= n; i++) {
-            dp[i][0] = i;
-        }
-        for(int i =0; i <= m; i++) {
-            dp[0][i] = i;
-        }
-        
-        for(int i =1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-                if(s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
-                else {
-                    dp[i][j] = 1 + Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1]));
-                }
-            }
-        }
-        return dp[n][m];
+        int[][] dp = new int[n][m];
+        for(int[] d: dp) Arrays.fill(d, -1);
+        return func(n-1, m-1, s1, s2, dp);
+    }
+    
+    int func(int i, int j, String s1, String s2, int[][] dp) {
+        if(i < 0) return j + 1;
+        if(j < 0) return i+1;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1.charAt(i) == s2.charAt(j)) return func(i-1, j-1, s1, s2, dp);
+        return dp[i][j] = 1 + Math.min(func(i, j-1, s1, s2, dp), Math.min(func(i-1, j, s1, s2, dp), func(i-1, j-1, s1, s2, dp)));
     }
 }
+
