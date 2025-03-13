c8cce952dfecf539e89dce76cb70792f02ba4f02
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-03-13 09:33:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,69 +1,76 @@
 //{ Driver Code Starts
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class gfg {
-    public static void main(String[] args) throws Exception {
+public class GFG {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int testCases = Integer.parseInt(br.readLine());
 
-        while (t-- > 0) {
-            int w = Integer.parseInt(br.readLine());
+        while (testCases-- > 0) {
+            int capacity = Integer.parseInt(br.readLine());
+            String[] valInput = br.readLine().split(" ");
+            String[] wtInput = br.readLine().split(" ");
 
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            List<Integer> array = new ArrayList<>();
+            int[] val = new int[valInput.length];
+            int[] wt = new int[wtInput.length];
 
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
+            for (int i = 0; i < valInput.length; i++) {
+                val[i] = Integer.parseInt(valInput[i]);
             }
 
-            int[] val = new int[array.size()];
-            int idx = 0;
-            for (int i : array) val[idx++] = i;
-
-            String lin = br.readLine();
-            String[] toke = lin.split(" ");
-            List<Integer> array1 = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : toke) {
-                array1.add(Integer.parseInt(token));
+            for (int i = 0; i < wtInput.length; i++) {
+                wt[i] = Integer.parseInt(wtInput[i]);
             }
 
-            int[] wt = new int[array1.size()];
-            idx = 0;
-            for (int i : array1) wt[idx++] = i;
-
-            // calling method knapSack() of class Solution
-            System.out.println(new Solution().knapSack(w, wt, val));
+            System.out.println(Solution.knapsack(capacity, val, wt));
+            System.out.println("~");
         }
     }
 }
+
 // } Driver Code Ends
 
 
 class Solution {
-    // Function to return max value that can be put in knapsack of capacity W.
-    static int knapSack(int W, int wt[], int val[]) {
-     int N = val.length;
-        int[][] dp = new int[N + 1][W + 1];
-
-        // Build the dp array
-        for (int i = 1; i <= N; i++) {
-            for (int w = 0; w <= W; w++) {
-                // If weight of the current item is less than or equal to the current capacity
-                if (wt[i - 1] <= w) {
-                    // Choose the maximum of including the item or not including it
-                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - wt[i - 1]] + val[i - 1]);
-                } else {
-                    // If the item cannot be included, carry forward the value without it
-                    dp[i][w] = dp[i - 1][w];
-                }
+    // Function to return max value that can be put in knapsack of capacity.
+    static int knapsack(int capacity, int val[], int wt[]) {
+        // code here
+        int n = val.length;
+        int dp[][] = new int[n][capacity+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<capacity+1;j++){
+                dp[i][j] = -1;
             }
-        
+        }
+        return recur(capacity, val, wt, n-1,dp);
     }
-     return dp[N][W];
-}
+    static int recur(int capacity, int val[], int wt[], int index, int dp[][]){
+        // base case
+        if(capacity == 0) {
+            dp[index][capacity] = 0;
+            return 0;
+        }
+        if(index==0){
+            if(wt[index]<=capacity){
+                dp[index][capacity] = val[index];
+                return val[index];
+            }else{
+                dp[index][capacity] = 0;
+                return 0;
+            }
+        }
+        if(dp[index][capacity]!=-1){
+            return dp[index][capacity];
+        }
+        int pick = 0;
+        if(wt[index] <= capacity){
+            pick = val[index] + recur(capacity - wt[index], val, wt, index-1, dp);
+        }
+
+        int noPick = 0 + recur(capacity, val, wt, index-1,dp);
+        dp[index][capacity] = Math.max(pick, noPick);
+        return dp[index][capacity];
+    }
 }
