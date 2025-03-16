4bd4aecfe644d7f08bde218abefc0e22f6991860
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-03-16 10:03:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.java
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -27,7 +27,7 @@ public class Main {
             for (int i : array) arr[idx++] = i;
 
             System.out.println(new Solution().minJumps(arr));
-            // System.out.println("~");
+            System.out.println("~");
         }
     }
 }
@@ -39,31 +39,48 @@ class Solution {
     static int minJumps(int[] arr) {
         int n = arr.length;
         
-        if (n == 0 || arr[0] == 0) {
-            return -1;
-        }
-        
-        if (n == 1) {
+        // If there are less than two elements, no jumps are needed
+        if (n <= 1) {
             return 0;
         }
         
-        int jumps = 0;
-        int currentEnd = 0;
-        int farthest = 0;
+        // If the first element is 0, it's not possible to move forward
+        if (arr[0] == 0) {
+            return -1;
+        }
         
-        for (int i = 0; i < n - 1; i++) {
-            farthest = Math.max(farthest, i + arr[i]);
+        // Initialize variables
+        int jumps = 1; // Start from the first jump
+        int maxReach = arr[0]; // The farthest we can reach
+        int steps = arr[0]; // The number of steps we can take
+        
+        // Traverse the array
+        for (int i = 1; i < n; i++) {
+            // If we reached the end of the array
+            if (i == n - 1) {
+                return jumps;
+            }
             
-            if (i == currentEnd) {
-                jumps++; 
-                currentEnd = farthest; 
-        
-                if (currentEnd >= n - 1) {
-                    return jumps;
+            // Update the maxReach
+            maxReach = Math.max(maxReach, i + arr[i]);
+            
+            // Decrease steps
+            steps--;
+            
+            // If steps become 0, we need to increase the jump count
+            if (steps == 0) {
+                jumps++;
+                
+                // If we can't move forward, return -1
+                if (i >= maxReach) {
+                    return -1;
                 }
+                
+                // Reset steps to the number of steps we can take from the current position
+                steps = maxReach - i;
             }
         }
         
-        return -1;
+        return -1; // This line will never be reached if the solution is possible
     }
 }
