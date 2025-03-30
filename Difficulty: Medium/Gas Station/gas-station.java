9a764be759927c60359725fc2544f9dcec041735
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-03-30 13:19:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            ArrayList<Integer> array2 = new ArrayList<Integer>();
+
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            line = read.readLine();
+            tokens = line.split(" ");
+            for (String token : tokens) {
+                array2.add(Integer.parseInt(token));
+            }
+
+            // ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] gas = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) gas[idx++] = i;
+
+            int[] cost = new int[array2.size()];
+            idx = 0;
+            for (int i : array2) cost[idx++] = i;
+
+            int ans = new Solution().startStation(gas, cost);
+
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int startStation(int[] gas, int[] cost) {
+        int n = gas.length;
+        
+        int sum = 0;
+        int start = 0;
+        int total = 0;
+        
+        for (int i = 0; i < 2 * n; i++) {
+            int index = i % n;
+            sum += gas[index] - cost[index];
+            total += gas[index] - cost[index];
+            
+            if (sum < 0) {
+                sum = 0;
+                start = i + 1;
+            }
+        }
+        
+        return total >= 0 ? start % n : -1;
+    }
+}
+
