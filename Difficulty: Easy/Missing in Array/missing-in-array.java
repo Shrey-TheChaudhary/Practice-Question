9efc02f8e3cf5efe6738ea901803dbac32841d4f
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-04-21 07:07:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,41 +1,32 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.io.*;
 import java.util.*;
 
-class GFG {
-
-    public static void main(String[] args) throws IOException {
+class Geeks {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] str = br.readLine().trim().split(" ");
-            int[] arr = new int[n - 1];
-            for (int i = 0; i < n - 1; i++) {
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
                 arr[i] = Integer.parseInt(str[i]);
             }
-            Solution sln = new Solution();
-            System.out.println(sln.missingNumber(n, arr));
+            System.out.println(new Solution().missingNum(arr));
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-// User function Template for Java
 class Solution {
-
-    // Note that the size of the array is n-1
-    int missingNumber(int n, int arr[]) {
-
-     int sum=0,expectedSum=n;
-        for(int i=0;i<n-1;i++){
-            sum+=arr[i];
-            expectedSum+=i+1;
-        }
-        return expectedSum-sum;
+    int missingNum(int arr[]) {
+        // code here
+        int sum=0;
+       for(int i=1;i<=arr.length;i++){
+           sum=sum^i^arr[i-1];
+       }
+       return sum^arr.length+1;
     }
 }
