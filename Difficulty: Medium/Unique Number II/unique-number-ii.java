85fb373b1873a84a46b9724d63182fa41dd0d7b9
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-04-23 07:12:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number II/unique-number-ii.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public int[] singleNum(int[] arr) {
+        // Code here
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        }
+        int res[]=new int[2];
+        int z=0;
+        for(int i:hm.keySet())
+        {
+            if(hm.get(i)==1)
+            res[z++]=i;
+        }
+        Arrays.sort(res);
+        return res;
+    }
+}
+
+
+//{ Driver Code Starts.
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            // int k = Integer.parseInt(br.readLine());
+            // Create Solution object and find closest sum
+            Solution ob = new Solution();
+            int[] ans = ob.singleNum(arr);
+
+            // Print the result as a space-separated string
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println(); // New line after printing the results
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
