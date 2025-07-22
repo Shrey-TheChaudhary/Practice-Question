196677de3ffb4194678b825f01f7292fa220e297
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-07-22 07:20:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public int missingNumber(int[] arr) {
+
+        // code here
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<=9;i++){
+
+            map.put(i,0);
+
+        }
+
+        for(int i=0;i<arr.length;i++){
+
+            
+
+            if(map.containsKey(arr[i])){
+
+                map.put(arr[i],map.get(arr[i])+1);
+
+            }
+
+            
+
+        }
+
+        int missing=-1;
+
+        for(int i=1;i<=9;i++){
+
+            if(map.get(i)==0){
+
+                missing=i;
+
+                break;
+
+            }
+
+        }
+
+        return missing;
+
+    }
+
+}
+
