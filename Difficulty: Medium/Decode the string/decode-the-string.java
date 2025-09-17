bed490843afd3d236c23efdbcec89fb75e2cfbc5
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-09-17 13:28:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.java
+++ Difficulty: Medium/Decode the string/decode-the-string.java
@@ -1,56 +1,46 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine());
-        while (t-- > 0) {
-            String s = in.readLine();
-
-            Solution ob = new Solution();
-            out.println(ob.decodeString(s));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
     static String decodeString(String s) {
-       Stack<Integer> nums = new Stack<>();
-        Stack<StringBuilder> st = new Stack<>();
-        StringBuilder current = new StringBuilder();
+        Stack<String> stack = new Stack<>();
         
-        int num = 0;
-        
-        for (char c : s.toCharArray()) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
             if (Character.isDigit(c)) {
-                num = num * 10 + (c - '0');
+                int start = i;
+                i += 1;
+                while (Character.isDigit(s.charAt(i))) i++;
+                stack.push(s.substring(start, i));
+                i--;
+                continue;
             }
-            else if (c == '[') {
-                nums.push(num);
-                st.push(current);
-                current = new StringBuilder();
-                num = 0;
-            }
-            else if (c == ']') {
-                StringBuilder temp = current;
-                current = st.pop();
-                int count = nums.pop();
-                current.append(String.valueOf(temp).repeat(count));
-            }
-            else {
-                current.append(c);
+            
+            if (c == ']') {
+                StringBuilder expandedString = new StringBuilder();
+                String temp;
+                
+                while (!(temp = stack.pop()).equals("[")) {
+                    expandedString.insert(0, temp);
+                }
+                
+                int count = Integer.parseInt(stack.pop());
+                StringBuilder finalString = new StringBuilder();
+                
+                while (count-- > 0) {
+                    finalString.append(expandedString);
+                }
+                
+                stack.push(finalString.toString());
+            } else {
+                stack.push(String.valueOf(c));
             }
         }
         
-        return current.toString();
+        StringBuilder solution = new StringBuilder();
+        
+        while (!stack.isEmpty()) {
+            solution.insert(0, stack.pop());
+        }
+        
+        return solution.toString();
     }
 }
