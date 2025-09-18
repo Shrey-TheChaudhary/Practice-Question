f49a359930b5800a6984cf7a6f4a13e382b74d1b
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-09-18 13:15:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.java
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.java
@@ -1,21 +1,42 @@
 class Solution {
-    public ArrayList<Integer> nextLargerElement(int[] arr) {
+    public ArrayList<Integer> nextGreater(int[] arr) {
         // code here
-        Stack<Integer> st=new Stack<>();
-        ArrayList<Integer> ds=new ArrayList<>();
-        int n=arr.length;
-        for(int i=2*n-1;i>=0;i--){
-            while(!st.isEmpty() && arr[i%n]>=st.peek()){
-                st.pop();
+         int n = arr.length;
+        
+        int max = -1, ind = -1;
+        for(int i =0; i < n; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+                ind = i;
             }
-            
-            if(i<n){
-                ds.add(st.isEmpty() ? -1 : st.peek());
-            }
-            
-            st.push(arr[i%n]);
         }
-        Collections.reverse(ds);
-        return ds;
+        
+        Stack<Integer> stk = new Stack<>();
+        stk.push(max);
+        ArrayList<Integer> ll = new ArrayList<>(Collections.nCopies(n, -1));
+        ll.set(ind, -1);
+        int i = ind -1;
+        while(i >= 0) {
+            while(!stk.isEmpty() && stk.peek() <= arr[i]) stk.pop();
+            int temp = arr[i];
+            if(!stk.isEmpty()) {
+                ll.set(i, stk.peek());
+            }
+            stk.push(arr[i]);
+            i--;
+        }
+        
+        i = n-1;
+        while(i > ind) {
+            while(!stk.isEmpty() && stk.peek() <= arr[i]) stk.pop();
+            int temp = arr[i];
+            if(!stk.isEmpty()) {
+                ll.set(i, stk.peek());
+            }
+            stk.push(arr[i]);
+            i--;
+        }
+        
+        return ll;
     }
 }
