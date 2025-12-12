ba9f20ada2b2390a83e8f4d32b952cb7bf644879
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-12-12 06:44:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> transpose(int[][] mat) {
+        // code here
+         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+         
+        for(int i = 0; i < mat.length; i++){
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int j = 0; j < mat[0].length; j++){
+                temp.add(mat[j][i]);
+            }
+            list.add(temp);
+        }
+        
+        return list;
+    }
+}
