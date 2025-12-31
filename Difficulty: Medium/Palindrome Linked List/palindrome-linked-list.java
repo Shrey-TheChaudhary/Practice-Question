20cf07227fd734aad0e4013a628f7dcc7b4834b8
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2025-12-31 11:50:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -1,114 +1,38 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class Driver_code {
-    static Node insert(Node head, int data) {
-        Node temp = new Node(data);
-        if (head == null) {
-            head = temp;
-            return head;
-        } else {
-            Node t = head;
-            while (t.next != null) {
-                t = t.next;
-            }
-            t.next = temp;
-        }
-        return head;
-    }
-
-    static void printList(Node head) {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            Node head = null;
-
-            String str[] = read.readLine().trim().split(" ");
-            int listSize = str.length;
-            for (int i = 0; i < listSize; i++) {
-                head = insert(head, Integer.parseInt(str[i]));
-            }
-            boolean f = new Solution().isPalindrome(head);
-
-            System.out.println(f ? "true" : "false");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-/* Structure of class Node is
-class Node
-{
-    int data;
-    Node next;
-
-    Node(int d)
-    {
-        data = d;
-        next = null;
-    }
-}*/
-
 class Solution {
-    // Function to check whether the list is palindrome.
-    boolean isPalindrome(Node head) {
-      if(head==null || head.next==null){
-           return true;
-       } 
-       
-        Node slow=head;
-        Node fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        
-        Node curr=slow;
-        Node prev=null;
-        Node next;
-        
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-        
-        Node right=prev;
-        Node left=head;
-        while(right!=null){
-            if(right.data!=left.data){
-                return false;
+    public boolean isPalindrome(Node head) {
+        // code here
+        if(head.next == null)   return true;
+
+        Node fast = head, slow = head, prev = null, nextStart = null;
+        while (fast != null) {
+            fast = fast.next;
+            if (fast == null) { // edge case -> odd no of items, dont consider middle
+                nextStart = slow.next;
+                slow = prev;
+                break;
             }
-            left=left.next;
-            right=right.next;
+            if(fast.next == null)    break;
+            fast = fast.next;
+
+            // reverse previous pointers of slow
+            Node temp = slow;
+            slow = slow.next;
+            temp.next = prev;
+            prev = temp;
         }
-        return true;
+
+        if(slow != null && nextStart == null) {
+            nextStart = slow.next;
+            slow.next = prev;
+        }
+
+        // simple check for palindrome
+        while (nextStart != null && slow != null) {
+            if (nextStart.data != slow.data)    return false;
+            nextStart = nextStart.next;
+            slow = slow.next;
+        }
+
+        return slow == null && nextStart == null;
     }
 }
