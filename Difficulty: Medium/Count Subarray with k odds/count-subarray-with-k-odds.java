137f92e0e4c9c48374562ae2a58937938824d350
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2026-01-08 10:35:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarray with k odds/count-subarray-with-k-odds.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSubarrays(int[] arr, int k) {
+        // code here
+         LinkedList<Integer>lt = new LinkedList<>();
+        int pr = -1,ans=0,n = arr.length;
+        for(int i=0;i<=n;i++){
+            if(i==n || arr[i]%2==1){
+                if(lt.size()==k){
+                    int f = lt.getFirst()-pr;
+                    int s = i-lt.getLast();
+                    ans+=f*s;
+                    pr=lt.removeFirst();
+                }
+                lt.add(i);
+            }
+        }
+        return ans;
+    }
+}
