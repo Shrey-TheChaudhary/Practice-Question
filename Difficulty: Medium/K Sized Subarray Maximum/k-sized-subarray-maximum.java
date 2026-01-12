55d13c128bdb54cbec8d6df97886772aee851a5f
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2026-01-12 08:32:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -1,64 +1,29 @@
-//{ Driver Code Starts
-// Initial template for JAVA
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        // taking input using class Scanner
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            // taking total number of elements
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-            int k = Integer.parseInt(br.readLine());
-            ArrayList<Integer> res = new Solution().maxOfSubarrays(arr, k);
-
-            // printing the elements of the ArrayList
-            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    public ArrayList<Integer> maxOfSubarrays(int arr[], int k) {
-         ArrayList<Integer> result = new ArrayList<>();
-        Deque<Integer> deque = new LinkedList<>();
-        for (int i = 0; i < arr.length; i++) {
-            // Remove indices of elements outside the current window
-            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
-                deque.pollFirst();
-            }
-            // Remove indices of elements that are smaller than the current element
-            while (!deque.isEmpty() && arr[deque.peekLast()] <= arr[i]) {
-                deque.pollLast();
-            }
-            deque.offerLast(i);
-            if (i >= k - 1) {
-                result.add(arr[deque.peekFirst()]);
-            }
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        return f(arr,k);
+    }
+    
+    ArrayList<Integer> f(int[] arr,int k){
+        Deque<Integer> dq=new ArrayDeque<>();
+        ArrayList<Integer> ans=new ArrayList<>();
+        int i=0;
+        for(i=0;i<k;i++){
+            while(dq.size()>0 &&dq.peekLast()<arr[i]) dq.removeLast();
+            dq.addLast(arr[i]);
         }
-        return result;
         
+        ans.add(dq.peekFirst());
+        
+       for(;i<arr.length;i++){
+           while(dq.size()>0 && dq.peekFirst()<arr[i-k]) dq.removeFirst();
+           if(dq.size()>0 && dq.peekFirst()==arr[i-k]) dq.removeFirst();
+           
+            while(dq.size()>0 && dq.peekLast()<arr[i]) dq.removeLast();
+            dq.addLast(arr[i]);
+            
+             ans.add(dq.peekFirst());
+            
+       }
+       return ans;
     }
 }
