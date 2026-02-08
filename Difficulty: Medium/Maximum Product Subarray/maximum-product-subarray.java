69c447b4ad19b7e3bbcc40d74a60761a077d94c9
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2026-02-08 08:14:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
@@ -1,42 +1,21 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
-            String[] inputLine = br.readLine().split(" ");
-            int n = inputLine.length;
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            System.out.println(new Solution().maxProduct(arr));
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to find maximum product subarray
     int maxProduct(int[] arr) {
         // code here
-        int n=arr.length;
-        int leftCurrent=1;
-        int rightCurrent=1;
-        int max=Integer.MIN_VALUE;
-        for(int i=0; i<n; i++){
-            leftCurrent *=arr[i];
-            rightCurrent *=arr[n-i-1];
-            max=Math.max(Math.max(leftCurrent,max),rightCurrent);
-            if(leftCurrent==0) leftCurrent=1;
-            if(rightCurrent==0) rightCurrent=1;
+        int n = arr.length;
+        int ma = arr[0];
+        int mi = ma;
+        int ans = ma;
+        for(int i = 1; i < n; i++){
+            if(arr[i] < 0){
+               int temp = ma;
+               ma = mi;
+               mi = temp;
+            }
+            ma = Math.max(arr[i], arr[i]*ma);
+            mi = Math.min(arr[i], arr[i]*mi);
+            ans = Math.max(ans, ma);
         }
-        return max;
+        
+        return ans;
     }
 }
