c384268fe0add37219e7c3cc003ec4992c08c319
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2026-02-20 07:35:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form the Largest Number/form-the-largest-number.java
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.java
@@ -1,26 +1,23 @@
 class Solution {
     public String findLargest(int[] arr) {
-        // code here
-        String [] str=new String[arr.length];
-        for(int i=0;i<arr.length;i++){
-            str[i]=""+arr[i];
+
+        String[] nums = new String[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            nums[i] = String.valueOf(arr[i]);
         }
-        Arrays.sort(str,(a,b)->{
-            String s=a+b;
-            String m=b+a;
-            return m.compareTo(s);
-        });
-        StringBuilder sb=new StringBuilder();
-        for(String item:str) sb.append(item);
-         boolean flag=true;
-         for(int i=0;i<sb.length();i++){
-             char ch=sb.charAt(i);
-             if(ch!='0'){
-                 flag=false;
-                 break;
-             }
-         }
-         if(flag==true) return "0";
-        return sb.toString();
+
+        Arrays.sort(nums, (a, b) -> (b + a).compareTo(a + b));
+
+        if (nums[0].equals("0")) return "0";
+
+       
+        StringBuilder result = new StringBuilder();
+        for (String s : nums) {
+            result.append(s);
+        }
+
+        return result.toString();
     }
 }
+
