eaae9e3d69477e1d4dfd457398b8ea8106aa9281
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2026-02-21 16:15:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.java
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -1,50 +1,32 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test_cases = Integer.parseInt(br.readLine().trim());
-
-        while (test_cases-- > 0) {
-            // Read the array from input line
-            String[] input = br.readLine().trim().split(" ");
-            int[] arr = new int[input.length];
-            for (int i = 0; i < input.length; i++) {
-                arr[i] = Integer.parseInt(input[i]);
-            }
-
-            // Solution instance to invoke the function
-            Solution ob = new Solution();
-            int result = ob.hIndex(arr);
-
-            System.out.println(result);
-            System.out.println("~");
-        }
+class Solution 
+{
+    
+    private int papers(int arr[], int H)
+    {
+        int c=0;
+        
+        for(int x:arr)
+            if(x>=H)
+                c++;
+        return c;
     }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-class Solution {
-    // Function to find hIndex
-  public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int n = citations.length;
-        int h_index = 0;
-
-        for (int i = 0; i < n; i++) {
-            int remainingPapers = n - i;
-            if (citations[i] >= remainingPapers) {
-                h_index = remainingPapers;
-                break;
+    
+    public int hIndex(int[] citations)
+    {
+        // code here
+        int low=1, high= citations.length, ans=0;
+        while(low<=high)
+        {
+            int mid= (low+high)/2; //H
+            if(papers(citations,mid)>=mid)
+            {
+                ans=mid;
+                low=mid+1;
             }
+            else
+                high=mid-1;
         }
-        return h_index;
+        
+        return ans;
     }
 }
